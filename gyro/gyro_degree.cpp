fb27c6f89da29cd929541c505e1d90015542b5f0
Richard Wendel <richard.wendel.3d@gmail.com>
2016-03-29 02:52:39 +0000

    Cleaned up gyro_degree.cpp with clear(er) instructions

--- gyro/gyro_degree.cpp
+++ gyro/gyro_degree.cpp
@@ -14,53 +14,38 @@
 #define GYRO_ADDRESS1 0x6A
 
 using namespace std;
-int main(){
-	int i2c_1;
-	int i2c_2;
-	i2c_1 = open("/dev/i2c-1", O_RDWR); 
-	i2c_2 = open("/dev/i2c-1", O_RDWR); 
+
+int open_i2c_port(int address){
+	int i2c = open("/dev/i2c-1", O_RDWR);
 	
-	if(i2c_1 < 0){
-		cout << "Something went wrong opening the i2c-1" <<endl;
-		cout << "Error code: " << errno << endl;
-		return 2;
-	}
-	if(i2c_2 < 0){
-		cout << "Something went wrong opening the i2c-2" <<endl;
+	if(i2c < 0){
+		cout << "Something went wrong opening the i2c port" <<endl;
 		cout << "Error code: " << errno << endl;
 		return 2;
 	}
 
-	if( ioctl( i2c_1, I2C_SLAVE, GYRO_ADDRESS0) < 0){
-		cout << "Failed to set GYRO0 slave address" <<endl;
+	if( ioctl( i2c, I2C_SLAVE, address) < 0){
+		cout << "Failed to set i2c (address: " << address << ") slave address" <<endl;
 		cout << "Error Code: " << cerr << endl;
 		return 2;
 	}
-	if( ioctl( i2c_2, I2C_SLAVE, GYRO_ADDRESS1) < 0){
-		cout << "Failed to set GYRO1 slave address" <<endl;
-		cout << "Error Code: " << cerr << endl;
-		return 2;
-	}
+	
+	return i2c;
+}
 
+int main(){
+	int i2c_1 = open_i2c_port(GYRO_ADDRESS0);
+	int i2c_2 = open_i2c_port(GYRO_ADDRESS1);
 	//Everything should be working, so lets transmit some data
 	
-	i2c_smbus_write_byte_data(i2c_1, 0x20, 0x0F); //set normal mode
-	i2c_smbus_write_byte_data(i2c_1, 0x23, 0x00); //set resolution to 245 dps
-	//i2c_smbus_write_byte_data(i2c_1, 0x24, 0x40); //turn on FIFO
-	//i2c_smbus_write_byte_data(i2c_1, 0x2E, 0x2F); //set fifo
-		////set fifo_ctrl to dynamic stream mode with fth to 15
+	i2c_smbus_write_byte_data(i2c_1, 0x20, 0x0F); //set normal mode (enable x,y,z axis) (set Hz to 100)
+	i2c_smbus_write_byte_data(i2c_1, 0x23, 0x00); //set resolution to 245 dps (sensitiity = 0.00875)
 	
-	//set fifo_ctrl to 11000100 -> dynamic stream mode, and seting fth to 4
-	//then need to check fifo_src(fth) if we have at least fth to read
-	//then read fifo_src(fss4:0)+1
-
-	//i2c_smbus_write_byte_data(i2c_2, 0x20, 0x0F);//set normal mode
-	//i2c_smbus_write_byte_data(i2c_2, 0x23, 0x00); //set resolution
-	
-	double xpos = 0;
+	double xpos = 0;//The roll/pitch/yaw values
 	double ypos = 0;
 	double zpos = 0;
-	uint8_t xhi0 =0; 
+
+	uint8_t xhi0 =0; //used when reading in data
 	uint8_t xlo0 =0;
 	uint8_t yhi0 =0;
 	uint8_t ylo0 =0;
@@ -71,7 +56,6 @@ int main(){
         int16_t zdata0 =0;
 	uint8_t fth = 0;
 
-	uint8_t * buff = new uint8_t[40];
 	for(int i =0; i<10000; i++){
 		fth = i2c_smbus_read_byte_data(i2c_1, 0x27);
 		if(fth & 8 == 8){
@@ -89,68 +73,13 @@ int main(){
 			xdata0 *= 0.00875;// multiplying by resolution
 			ydata0 *= 0.00875;
 			zdata0 *= 0.00875;	
-			
 	
-			xpos += xdata0 *0.01;
+			xpos += xdata0 *0.01;// multiplying by frequency (default is 100 Hz)
 			ypos += ydata0 *0.01;
 			zpos += zdata0 *0.01;
 
-
-			cout << "x: "<< xpos<< "\ty: " <<ypos<< "\tz: " <<zpos<<endl; //"\t"<< xdata1<< endl;
-			//cout << "x: "<< xdata0<< "\ty: " <<ydata0 << "\tz: " <<zdata0<<endl; //"\t"<< xdata1<< endl;
 		}
-//		fth = i2c_smbus_read_byte_data(i2c_1, 0x2F);
-//		cout << "fth value" << int(fth)<<endl;
-//		if((fth & 0x80) == 0x80){//if there are enough data to read
-//			for(uint8_t i = fth & 16; i>0; i--){//get the level of data samples to read
-//				xhi0 = i2c_smbus_read_byte_data(i2c_1, 0x29);
-//				xlo0 = i2c_smbus_read_byte_data(i2c_1, 0x28);
-//				yhi0 = i2c_smbus_read_byte_data(i2c_1, 0x2B);
-//				ylo0 = i2c_smbus_read_byte_data(i2c_1, 0x2A);
-//				zhi0 = i2c_smbus_read_byte_data(i2c_1, 0x2D);
-//				zlo0 = i2c_smbus_read_byte_data(i2c_1, 0x2C);
-//				
-//				int16_t xdata0 = (int16_t)(xlo0 | (xhi0 << 8));
-//				int16_t ydata0 = (int16_t)(ylo0 | (yhi0 << 8));
-//				int16_t zdata0 = (int16_t)(zlo0 | (zhi0 << 8));
-//				
-//				xdata0 *= 0.00875;
-//				ydata0 *= 0.00875;
-//				zdata0 *= 0.00875;	
-//
-//				cout << "x: "<< xdata0<< "\ty: " <<ydata0 << "\tz: " <<zdata0<<endl; //"\t"<< xdata1<< endl;
-//				
-//			}
-			
-				
-			//int bytes_read = i2c_smbus_read_block_data(i2c_1,0x28,buff);
-			//read_block_data causes kernal panic! Yay!
-
-			//cout << "read_bytes: "<<bytes_read<<endl;
-			//for(int j = 0; j< bytes_read; j++){
-			//	cout << int(buff[j]) <<endl;
-			//}
-//		}
-		
-	//	uint8_t xhi0 = i2c_smbus_read_byte_data(i2c_1, 0x29);
-	//	uint8_t xlo0 = i2c_smbus_read_byte_data(i2c_1, 0x28);
-	//	uint8_t yhi0 = i2c_smbus_read_byte_data(i2c_1, 0x2B);
-	//	uint8_t ylo0 = i2c_smbus_read_byte_data(i2c_1, 0x2A);
-	//	uint8_t zhi0 = i2c_smbus_read_byte_data(i2c_1, 0x2D);
-	//	uint8_t zlo0 = i2c_smbus_read_byte_data(i2c_1, 0x2C);
-	//	
-	//	int16_t xdata0 = (int16_t)(xlo0 | (xhi0 << 8));
-	//	int16_t ydata0 = (int16_t)(ylo0 | (yhi0 << 8));
-	//	int16_t zdata0 = (int16_t)(zlo0 | (zhi0 << 8));
-	//	
-	//	xdata0 *= 0.00875;
-	//	ydata0 *= 0.00875;
-	//	zdata0 *= 0.00875;	
-	//	
-	//	//float xfloat_data0 = xdata0 * 0.00875;
-	//	cout << "x: "<< xdata0<< "\ty: " <<ydata0 << "\tz: " <<zdata0<<endl; //"\t"<< xdata1<< endl;
-		
-		//for(int j = 0; j <100; j++){};//the best way to "wait"
 	}
-	delete buff;
+
+	cout << "x: "<< xpos<< "\ty: " <<ypos<< "\tz: " <<zpos<<endl; //"\t"<< xdata1<< endl;
 }
