7ae5b4fe74fcdb5e342265f0fe12248cf89834ff
rgw3d <rgw3d@aol.com>
2016-08-26 00:31:41 -0500

    Cleaning up master branch

--- UDPserverSEND.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
-MESSAGE = "Hello, World!"
-
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
