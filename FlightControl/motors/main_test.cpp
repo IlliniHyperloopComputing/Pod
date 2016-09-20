89d9274c7e85185b3365993d486afaaa043fe484
Mark Schmidt <schmidtwmark@gmail.com>
2016-09-19 19:57:30 -0500

    main

--- FlightControl/motors/main_test.cpp
+++ FlightControl/motors/main_test.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include "motors.h"
 
+using namespace std;
 int main(){
 
 	motor_control x(BlackLib::pwmName::P9_16, 1000, 700, 700, 2000);
-	BlackLib::BlackPWM * y = x.tmp_testing_get_pwm_ptr();
-
-	std::cout << "helllooo" << std::endl;
-	std::cout << y->getNumericDutyValue() << std::endl;
+	BlackLib::BlackPWM * y = x.tmp_testing_get_pwm_ptr();		
+	
+	for(float i  = 0; i <= 100; i+= 0.5){
+		cout << "i = " << i << endl;
+		x.raise_by_percent(i);	
+	}
 
 }
