f28a641f87bdd15bbd69fffbf01a15bdfe6ce711
Badtoasters <curtislowder@gmail.com>
2016-09-26 19:18:09 -0500

    brakes

---
+++ FlightControl/brakes/brakes.h
@@ -0,0 +1,37 @@
+
+#ifndef BRAKES_H
+#define BRAKES_H
+
+#include <stdint.h>
+#include "../../BlackLib/v3_0/BlackPWM/BlackPWM.h"
+
+// brake control. 
+// when this object is initialized, it has the ability to control the brakes
+// Controls both the switch and the flipflop for our brake controls
+
+class brake_control{
+	public:
+		// Constructor
+		// Takes in two pins which control the power and the flipflop
+		// Power controls whether or not the brakes will be operable
+		// Flip flop controls the direction the brakes will move
+		//	off is forward, on is backward
+		motor_control(enum BlackLib::pwmName power, enum BlackLib::pwmName flip_flop);
+
+		// Turn on power, turn off flipflop
+		void forward();
+
+		// Turn on power, turn on flipflop
+		void backward();
+
+		// turn off power, turn off flipflop
+		void stop();
+		
+	private:
+
+		enum BlackLib::pwnName power;
+		enum BlackLib::pwnName flip_flop;
+
+}
+
+#endif
