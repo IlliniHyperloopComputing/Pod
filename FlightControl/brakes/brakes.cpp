08012f6a4418d66daf49b5e3a5b05be34bd037c7
Badtoasters <curtislowder@gmail.com>
2016-10-03 20:04:44 -0500

    Stuff

--- FlightControl/brakes/brakes.cpp
+++ FlightControl/brakes/brakes.cpp
@@ -1,27 +1,27 @@
-include "motors.h"
+#include "brakes.h"
 
 using namespace BlackLib;
 
-motor_control(enum BlackLib::gpioName power, enum BlackLib::gpioName flip_flop)
-			: power(power) , flip_flop(flip_flop)
+brake_control::brake_control(BlackLib::gpioName power, BlackLib::gpioName flip_flop)
+			: power(power,output, FastMode) , flip_flop(flip_flop,output,FastMode)
 {
 	
 }
 
 // Turn on power, turn off flipflop
-void forward() {
+void brake_control::forward() {
 	power.setValue( BlackLib::high );
 	flip_flop.setValue( BlackLib::low );
 }
 
 // Turn on power, turn on flipflop
-void backward() {
+void brake_control::backward() {
 	power.setValue( BlackLib::high );
 	flip_flop.setValue( BlackLib::high );
 }
 
 // turn off power, turn off flipflop
-void stop() {	
+void brake_control::stop() {	
 	power.setValue( BlackLib::low );
 	flip_flop.setValue( BlackLib::low );
 }
