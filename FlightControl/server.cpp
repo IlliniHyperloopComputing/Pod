c309832c53fb0e7ad53577984f177cb1b07ec449
Richard Wendel <rwendel2@illinois.edu>
2016-10-10 19:28:26 -0500

    the big makefile update

--- FlightControl/server.cpp
+++ FlightControl/server.cpp
@@ -1,11 +1,13 @@
 #include "server.h"
 
+typedef boost::shared_ptr<tcp_connection> pointer;
+
 ////////////////
 ////////////////
 ////////////////
 //tcp_connection
-static pointer tcp_connection::create(boost::asio::io_service & io_service){
-    return pointer(new tcp_connection(io_service));
+pointer tcp_connection::create(boost::asio::io_service & io_service){
+    return pointer(new tcp_connection(io_service, true));
 }
 
 tcp::socket& tcp_connection::socket(){
@@ -15,34 +17,42 @@ tcp::socket& tcp_connection::socket(){
 void tcp_connection::start(){
 
     //create message here-- must be class variable/persist
-    std::string message_;
-    message_ = "wow"
+    message_ = "wow";
     
+    //begin writing data
     boost::asio::async_write(socket_, boost::asio::buffer(message_),
             boost::bind(&tcp_connection::handle_write, shared_from_this(),
             boost::asio::placeholders::error,
             boost::asio::placeholders::bytes_transferred));
-    boost::asio::async_read_until(socket_, , "\n",
+
+    //Wait for commands
+    boost::asio::async_read_until(socket_,read_buffer_, "\n",
             boost::bind(&tcp_connection::handle_read, shared_from_this(),
             boost::asio::placeholders::error,
             boost::asio::placeholders::bytes_transferred));
 
-    socket_.async_read_until(boost::asio::buffer(data_
 }
 
-tcp_connection::tcp_connection(boost::asio::io_service& io_service)
-            : socket_(io_service)
+tcp_connection::tcp_connection(boost::asio::io_service& io_service, bool no_delay)
+            : socket_(io_service), no_delay_(no_delay)
     {}
 void tcp_connection::handle_write(const boost::system::error_code&,size_t){
-    //handle that write, yo
-
     //get data and then write again
+    
+    
 }
 
-void tcp_connection::handle_read(const boost:system::error_code&, std::size_t bytes_transferred){
+void tcp_connection::handle_read(const boost::system::error_code&, std::size_t bytes_transferred){
+    //
     
 
+}
 
+std::string tcp_connection::buffer_to_string(boost::asio::streambuf& read_buffer){
+    std::string line(
+            boost::asio::buffers_begin(read_buffer.data()),
+            boost::asio::buffers_end(read_buffer.data()));
+    return line;
 }
 
 
--- FlightControl/server.h
+++ FlightControl/server.h
@@ -20,10 +20,15 @@ class tcp_connection : public boost::enable_shared_from_this<tcp_connection>
         void start();
     
     private:
-        tcp_connection(boost::asio::io_service& io_service);
+        tcp_connection(boost::asio::io_service& io_service,bool no_delay);
         void handle_write(const boost::system::error_code& , size_t);
+        void handle_read(const boost::system::error_code&, size_t bytes_transferred);
+        std::string buffer_to_string(boost::asio::streambuf& read_buffer);
 
         tcp::socket socket_;
+        tcp::no_delay no_delay_;
+        std::string message_;
+        boost::asio::streambuf read_buffer_;
 };
 
 class tcp_server{
@@ -36,7 +41,6 @@ class tcp_server{
                 const boost::system::error_code&error);
         
         tcp::acceptor acceptor_;
-        std::string message_;
         //std::string
 
 };
