2a5afa03f8efdd68a71c4596f85f2ee18e45fafd
Mark Schmidt <markschmidt@debian-64-rgw3d.rgw3d>
2016-11-06 11:33:30 -0600

    Motors cleanup

--- FlightControl/motors.cpp
+++ FlightControl/motors.cpp
@@ -12,55 +12,9 @@ motor_control::motor_control(enum BlackLib::pwmName pwm_pin, double initial_us, 
 }
 
 
-void motor_control::raise_by_percent(double percent){
-	double change_us = ( percent * ( high_us - low_us ) );
-	double new_us = current_us + change_us;
-	
-	if ( new_us > high_us ) {
-		new_us = high_us;
-	}
-
-
-	if ( new_us < low_us ) {
-		new_us = low_us;
-	}
-	
-	set_microseconds( new_us );
-}
-
-void motor_control::lower_by_percent(double percent){
-	double change_us = ( percent *( high_us - low_us ) );
-	double new_us = current_us - change_us;
-	
-	if ( new_us > high_us ) {
-		new_us = high_us;
-	}
-	
-	if ( new_us < low_us ) {
-		new_us = low_us;
-	}
-	
-	set_microseconds( new_us );
-}
-
-// Updates the percentage, doesn't control pod
-// Sets percentage of the range of possible duty cycle values
-void motor_control::update_percent( double new_us ) {
-	percent = ( new_us - low_us ) / ( high_us - low_us );
-}
-
-double motor_control::get_percent(){
-	return percent;
-}
-
-double motor_control::get_microseconds(){
-	return current_us;
-}
-
 void motor_control::arm(){
 	is_armed = true;	
 	set_microseconds( arm_us );
-	update_percent( arm_us );
 }
 
 void motor_control::disarm(){
@@ -81,27 +35,13 @@ void motor_control::set_low(){
 }
 
 void motor_control::set_microseconds(double microseconds){	
+/*
     pwm.setDutyPercent(100.0);
 	pwm.setPeriodTime(uint64_t(20), BlackLib::milisecond);
+*/
 
     double x = microseconds / 200.0;//FUCK THIS STUPID FUCKING LINE
     pwm.setDutyPercent(x);
-    //pwm.setLoadRatioTime(microseconds, BlackLib::microsecond);
-    std::cout<< pwm.getNumericValue() << std::endl;
-    std::cout<< pwm.getDutyValue() << std::endl;
-    std::cout<< pwm.getPolarityValue()<<std::endl;
-//    if(pwm.getPolarityValue() == "1"){
-//        pwm.tooglePolarity();
-//    }
-	//update_percent( microseconds );
-	//current_us = microseconds;
-	//pwm.setDutyPercent(create_percent(microseconds));
 
 }
 
-double motor_control::create_percent(double microseconds_in){
-	
-	double percent_us = ( microseconds_in - low_us ) / ( high_us - low_us );
-
-	return percent_us;
-}
