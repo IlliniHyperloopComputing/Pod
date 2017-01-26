1e53b1c501d65c830daffc6bbdf80ec1c0f6ee80
hyperloop <hyperloop@debian-64-rgw3d.rgw3d>
2017-01-25 23:27:04 -0600

    major reworks

--- FlightControl/events.hpp
+++ FlightControl/events.hpp
@@ -1,29 +1,20 @@
 #ifndef FSM_EVENTS
 #define FSM_EVENTS
 
-struct event{};
-struct flight_coast {};
 struct flight_brake {};
-struct init_sensors {};//used to start sensor orthagonal machine
 
 // A "complicated" event type that carries some data.
 enum IncomingCommandEnum
 {
     SAFE_MODE=0,
-    INIT_SENSORS=1,
-    FUNCT_A=2,
-    FUNCT_B=3,
-    FUNCT_C=4,
-    FUNCT_D=5,
-    LOADING=6,
-    FLIGHT_A=7,
-    E_BRAKE=8,
-    LEV_MOTOR=9,
-    STA_MOTOR = 10,
-    ARM_LEV_MOTOR = 11,
-    ARM_STA_MOTOR = 12,
-    OFF = 13,
-    BRAKE = 14
+    FUNCTIONAL_TEST=1,
+	FLIGHT=2,
+	BRAKING=3, 
+	LEV_MOTOR=4,
+	STA_MOTOR=5,
+	ARM_LEV_MOTOR=6,
+	ARM_STA_MOTOR=7,
+	
 };
 /*
 struct user_select
