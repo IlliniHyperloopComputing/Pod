fee4f5577db3451d1dc6974b7c7160103b2dd9ab
Mark Schmidt <mwschmi2@illinois.edu>
2017-02-11 18:11:37 -0600

    Sensor classes set up

---
+++ CentralComputing/Sensor_Test.cpp
@@ -0,0 +1,23 @@
+#include "Sensor.h"
+#include <iostream>
+#include <unistd.h>
+
+
+using namespace std;
+
+int main() {
+	//set up configuration
+	Sensor_Configuration c;
+	c.type = THERMOCOUPLE;
+	c.simulation = 1;
+	c.count = 4;
+
+	vector<Sensor_Configuration> configs;
+	configs.push_back(c);
+
+	Sensor_Package p(configs);
+	usleep(20000000);
+	p.update();
+	
+	cout << "Mostly works!" << endl;
+}
---
+++ CentralComputing/Thermocouple.cpp
@@ -0,0 +1,48 @@
+#include "Sensor.h"
+#include <iostream>
+
+using namespace std;
+
+Thermocouple::Thermocouple(Sensor_Configuration configuration) : Sensor_Group(configuration) {
+	
+}
+
+/*Thermocouple::~Thermocouple(){
+	//do nothing
+}*/
+
+bool Thermocouple::connect() {
+	//TODO::implement connection
+	return true;
+}
+
+void Thermocouple::reset() {
+	//TODO: implement resetting
+}
+
+void Thermocouple::update() {
+
+	switch(simulation) {
+		case 0:
+			//TODO: read from XMega
+			break;
+		case 1:
+			simulation_1();
+			break;
+	}
+}
+
+void Thermocouple::simulation_1() {
+	auto start = Sensor_Package::start_time;
+	auto now = Sensor_Package::get_current_time();
+	auto difference = now - start;
+	cout << difference << endl;
+	
+	//TODO calc some actual value
+	
+	//TODO: lock mutex
+	for(size_t i = 0; i < data.size(); i++) {
+		data[i] = i;
+	}
+	//TODO: unlock mutex
+}
