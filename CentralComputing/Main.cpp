c3263660411a8f50a1cc11b804fa285fb2fe4975
Richard Wendel <rwendel2@illinois.edu>
2017-03-29 21:29:44 -0500

    Starting basic testing of Spi class. So far, able to open spidev1.x as anticipated. Also, able to create device tree entry on demand if not already present

--- CentralComputing/Main.cpp
+++ CentralComputing/Main.cpp
@@ -1,4 +1,6 @@
+#include "Spi.h"
 #include "Sensor_Test.h"
+
 int main(int argc, char** argv) {
 
   #ifdef TEST_POD
@@ -6,6 +8,12 @@ int main(int argc, char** argv) {
     return 0;
   #else
     //Define normal run mode
+    uint8_t bpi[] = {2,2};
+    Xmega_Setup x1 = {"/dev/spidev1.0", 2, bpi, 500000, 8};
+    Xmega_Setup x2 = {"/dev/spidev1.1", 2, bpi, 500000, 8};
+    Spi spi(&x1, &x2);
+      
+      
   #endif
 
 
