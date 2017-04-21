baa7246063f0eddc1c4bd89f46d1cd0469b8480b
Richard Wendel <rwendel2@illinois.edu>
2017-04-20 23:30:17 -0500

    Working quite wellgit add UnitTests/Spi_Test.cpp  SPI request types are all working well. Able to recieve Sensor_status, state, sensors, all, or none!

--- CentralComputing/UnitTests/Spi_Test.cpp
+++ CentralComputing/UnitTests/Spi_Test.cpp
@@ -7,7 +7,6 @@ int Spi_Test::test(int argc, char** argv){
 
   print_test("%s\t\t%s\t%c\n",argv[0], argv[1], argv[1][0]);
   if(argv[1][0] == '1'){
-    print_test("I am in the correct function");
     uint8_t bpi1[] = {2,2,2,2,2,2,2,2,2,2};
     Xmega_Setup x1 = {"/dev/spidev1.0", 10, bpi1, 500000, 8};
     uint8_t bpi2[] = {2,2,2};
@@ -26,7 +25,7 @@ int Spi_Test::test(int argc, char** argv){
       clock_gettime(CLOCK_MONOTONIC, &ts2);
       if(result & 0b01) err1++;
       if(result & 0b10) err2++;
-      usleep(4000);
+      usleep(2500);
       elapsed += (ts2.tv_sec - ts.tv_sec) + (ts2.tv_nsec-ts.tv_nsec)/1000000000.0;
     }
     print_test("time elapsed: %lf\t Average time: %lf\n",elapsed, elapsed/iters);
@@ -38,6 +37,52 @@ int Spi_Test::test(int argc, char** argv){
     int16_t adc1 = spi.get_data(1,0);
     print_test("Got idx0: %d\n", adc1);
 
+  }
+  else if(argv[1][0] == '2'){
+    uint8_t bpi1[] = {2,2,2,2,2,2,2,2,2,2};
+    Xmega_Setup x1 = {"/dev/spidev1.0", 10, bpi1, 500000, 8};
+    uint8_t bpi2[] = {2,2,2};
+    Xmega_Setup x2 = {"/dev/spidev1.1", 3, bpi2, 500000, 8};
+    Spi spi(&x1, &x2);
+    Xmega_Transfer xt = {1,X_C_NONE, X_R_STATE};
+      
+    print_test("Asking for state\n");
+    int result = spi.transfer(xt);
+    print_test("Result: %d\n", result);
+
+    print_test("\nAsking for sensor status\n");
+    xt.req = X_R_SENSOR_STATUS;
+    result = spi.transfer(xt);
+    print_test("Result: %d\n", result);
+
+    print_test("\nAsking for NONE\n");
+    xt.req = X_R_NONE;
+    result = spi.transfer(xt);
+    print_test("Result: %d\n", result);
+
+    print_test("\nAsking for ALL\n");
+    xt.req = X_R_ALL;
+    result = spi.transfer(xt);
+    print_test("Result: %d\n", result);
+
+    print_test("\nSensor_status: %d\n", spi.get_sensor_status(1));
+    print_test("State: %d\n", spi.get_state(1));
+    for(int i = 0; i < 3; i++){
+      print_test("got data: idx: %x\t data:%x\tdata:%x\n",i, spi.get_data(0,i), spi.get_data(1,i));
+    }
+
+    print_test("\nAsking for SENSOR\n");
+    xt.req = X_R_SENSOR;
+    result = spi.transfer(xt);
+    print_test("Result: %d\n", result);
+
+    print_test("\nSensor_status: %d\n", spi.get_sensor_status(1));
+    print_test("State: %d\n", spi.get_state(1));
+    for(int i = 0; i < 3; i++){
+      print_test("got data: idx: %x\t data:%x\tdata:%x\n",i, spi.get_data(0,i), spi.get_data(1,i));
+    }
+
+    
   }
   else{
     uint8_t bpi1[] = {2,2,2,2,2,2,2,2,2,2};
