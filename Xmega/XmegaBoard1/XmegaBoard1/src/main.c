d2380061d2e7686a19ae91377bccd4cfe44f65c3
rgw3d <rgw3d@aol.com>
2017-08-02 03:19:55 -0600

    Fixed the optical encoder inputs. Only use PK2 and PF2 but that is alright for now. Only other thing to do is to put these routines into their own timer thread

--- Xmega/XmegaBoard1/XmegaBoard1/src/main.c
+++ Xmega/XmegaBoard1/XmegaBoard1/src/main.c
@@ -11,6 +11,8 @@
 #include "spi_to_bbb.h"
 #include "i2c_sensors.h"
 
+#define COOLDOWN 20
+
 
 //Sensor data storage
 //spi_to_bbb externs these variables
@@ -47,6 +49,11 @@ volatile uint32_t delta_2 = UINT32_MAX;
 volatile uint32_t rotation_count_1 = 0;
 volatile uint32_t rotation_count_2 = 0;
 
+uint8_t high_1 = 1;
+uint8_t high_2 = 1;
+uint8_t cooldown_1 = 0;
+uint8_t cooldown_2 = 0;
+
 uint32_t true_delta = UINT32_MAX;
 uint32_t true_rotation_count = 0;
 
@@ -55,12 +62,14 @@ ISR(PORTK_INT0_vect){
 	delta_1 = retro_1_time - retro_1_time_old;
 	retro_1_time_old = retro_1_time;
 	rotation_count_1++;
+	PORTK.INTFLAGS = 0x1;
 }
 ISR(PORTF_INT0_vect){
 	retro_2_time = rtc_get_time();
 	delta_2 = retro_2_time - retro_2_time_old;
 	retro_2_time_old = retro_2_time;
 	rotation_count_2++;
+	PORTF.INTFLAGS = 0x1;
 }
 
 int main (void)
@@ -70,13 +79,15 @@ int main (void)
 	rtc_init();	
 	init_spi_to_bbb();	//Setup SPI on Port C
 	
-	ioport_configure_port_pin(&PORTK, PIN2_bm, IOPORT_DIR_INPUT | IOPORT_SENSE_RISING);
-	PORTK.INT0MASK = PIN2_bm;
-	PORTK.INTCTRL =	PORT_INT0LVL_MED_gc;
+	//ioport_configure_port_pin(&PORTK, PIN2_bm, IOPORT_DIR_INPUT | IOPORT_SENSE_RISING);
+	//PORTK.INT0MASK = PIN2_bm;
+	//PORTK.INTCTRL =	PORT_INT0LVL_MED_gc;
+	ioport_configure_port_pin(&PORTK, PIN2_bm, IOPORT_DIR_INPUT | IOPORT_PULL_DOWN);
 	
-	ioport_configure_port_pin(&PORTF, PIN2_bm, IOPORT_DIR_INPUT | IOPORT_SENSE_RISING);
-	PORTF.INT0MASK = PIN2_bm;
-	PORTF.INTCTRL =	PORT_INT0LVL_MED_gc;
+	//ioport_configure_port_pin(&PORTF, PIN2_bm, IOPORT_DIR_INPUT | IOPORT_SENSE_RISING);
+	//PORTF.INT0MASK = PIN2_bm;
+	//PORTF.INTCTRL =	PORT_INT0LVL_MED_gc;
+	ioport_configure_port_pin(&PORTF, PIN2_bm, IOPORT_DIR_INPUT | IOPORT_PULL_DOWN);
 	
 	PMIC.CTRL |= PMIC_MEDLVLEN_bm;
 	PMIC.CTRL |= PMIC_LOLVLEN_bm;
@@ -102,14 +113,49 @@ int main (void)
 		
 		if(spi_transfer == 0){//Do anything that is not SPI related
 			
+			uint8_t val_1 = (PORTK.IN & PIN2_bm) >> PIN2_bp;
+			
+			if(val_1 && !high_1 ){
+				retro_1_time = rtc_get_time();
+				delta_1 = retro_1_time - retro_1_time_old;
+				retro_1_time_old = retro_1_time;
+				rotation_count_1++;
+				high_1 = 1;
+			}
+			if(!val_1 && high_1){
+				cooldown_1++;
+				if(cooldown_1 > COOLDOWN){
+					high_1 = 0;
+					cooldown_1 = 0;
+				}
+			}
+			if(spi_isr) continue;
+			
+			uint8_t val_2 = (PORTF.IN & PIN2_bm) >> PIN2_bp;
+			
+			if(val_2 && !high_2 ){
+				retro_2_time = rtc_get_time();
+				delta_2 = retro_2_time - retro_2_time_old;
+				retro_2_time_old = retro_2_time;
+				rotation_count_2++;
+				high_2 = 1;
+			}
+			if(!val_2 && high_2){
+				cooldown_2++;
+				if(cooldown_2 > COOLDOWN){
+					high_2 = 0;
+					cooldown_2 = 0;
+				}
+			}
+			
+			if(spi_isr) continue;
+			
 			if(delta_1 < delta_2){
 				true_delta = delta_1;
 			}
 			else{
 				true_delta = delta_2;
-			}	
-			if(spi_isr) continue;
-			
+			}				
 			if(rotation_count_1 > rotation_count_2){
 				true_rotation_count = rotation_count_1;
 			}
