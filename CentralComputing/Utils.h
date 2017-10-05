b93920c8d11cb2553c22143cb2422cdc1c47b3cb
Richard Wendel <rgw3d@aol.com>
2017-10-04 23:10:36 -0500

    Moved all new_headers to parent directory, and it all compiles. Also, greatly improved the Makefile

--- CentralComputing/Utils.h
+++ CentralComputing/Utils.h
@@ -1,5 +1,26 @@
+#ifndef UTILS_H
+#define UTILS_H
+#include <cmath>
+#include <algorithm>
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
+/**
+* Gets the current time since program startup
+* @return a long long representing the number of microseconds since startup
+**/
+long long get_elapsed_time();
+
+/**
+* Wrapper for printf, prints only in debug mode
+**/
 void print_debug(const char * format, ...);
+
+/**
+* Wrapper for printf, prints only in test mode
+**/
 void print_test(const char * format, ...);
+
+#endif // UTILS_H
