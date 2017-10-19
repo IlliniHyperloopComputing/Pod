4ddd9752fb401f3253a32b491b428aa6edc9493f
shreychowdhary <shrey.chowdhary1@gmail.com>
2017-10-18 19:18:24 -0500

    changed temp sensor names

--- CentralComputing/Sensor_Aux/Temperature.cpp
+++ CentralComputing/Sensor_Aux/Temperature.cpp
@@ -7,10 +7,10 @@ using namespace std;
 
 void temperature_parse(uint8_t * buffer, Arbitrary_Data raw) {
 	Temperature_Raw * t_raw = (Temperature_Raw *)raw.data;
-	t_raw->temperature_brakes = (uint8_t) *(buffer + INITIAL_TEMPERATURE_OFFSET);
-	t_raw->temperature_batteries = (uint8_t) *(buffer + INITIAL_TEMPERATURE_OFFSET + TEMPERATURE_OFFSET);
-	t_raw->temperature_cpu = (uint8_t) *(buffer + INITIAL_TEMPERATURE_OFFSET + 2*TEMPERATURE_OFFSET);
-	t_raw->temperature_motor = (uint8_t) *(buffer + INITIAL_TEMPERATURE_OFFSET + 3*TEMPERATURE_OFFSET);
-	t_raw->temperature_wheels = (uint8_t) *(buffer + INITIAL_TEMPERATURE_OFFSET + 4*TEMPERATURE_OFFSET);
+	t_raw->brake_temp = (uint8_t) *(buffer + INITIAL_TEMPERATURE_OFFSET);
+	t_raw->battery_temp = (uint8_t) *(buffer + INITIAL_TEMPERATURE_OFFSET + TEMPERATURE_OFFSET);
+	t_raw->cpu_temp = (uint8_t) *(buffer + INITIAL_TEMPERATURE_OFFSET + 2*TEMPERATURE_OFFSET);
+	t_raw->motor_temp = (uint8_t) *(buffer + INITIAL_TEMPERATURE_OFFSET + 3*TEMPERATURE_OFFSET);
+	t_raw->wheel_temp = (uint8_t) *(buffer + INITIAL_TEMPERATURE_OFFSET + 4*TEMPERATURE_OFFSET);
 
 }
