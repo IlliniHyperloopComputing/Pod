3ee1e0de807c3fa5c40ef2300560845ba999cc4c
shreychowdhary <shrey.chowdhary1@gmail.com>
2017-10-18 19:13:54 -0500

    converted temperature to multiple sensors

--- CentralComputing/Sensor_Aux/Temperature.h
+++ CentralComputing/Sensor_Aux/Temperature.h
@@ -3,16 +3,13 @@
 #include "../Data.h"
 
 struct Temperature_Raw {
-	uint8_t temperature_ext;
-	uint8_t temperature_int;
+	uint8_t temperature_brakes;
+	uint8_t temperature_batteries;
+	uint8_t temperature_cpu;
+	uint8_t temperature_motor;
+	uint8_t temperature_wheels;
 }
 
-struct Temperature_Calc {
-	double temperature_ext;
-	double temperature_int;
-}
-
-Arbitrary_Data temperature_calculation(Arbitrary_Data raw);
 void temperature_parse(uint8_t * buffer, Arbitrary_Data raw);
 
 #endif
