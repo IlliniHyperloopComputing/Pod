5938655a2074293345a71c048b83ea41761a3bc2
markschmidt <mwschmi2@illinois.edu>
2017-12-07 06:55:00 -0600

    Updated state machine

--- CentralComputing/Pod_State.cpp
+++ CentralComputing/Pod_State.cpp
@@ -1,4 +1,5 @@
 #include "Pod_State.h"
+#include "Simulation.h"
 
 // returns the current state as a E_States enum
 Pod_State::E_States Pod_State::get_current_state() {
@@ -9,26 +10,12 @@ Pod_State::E_States Pod_State::get_current_state() {
  * User controlled movement events
 **/
 
-void Pod_State::move_safety_setup() {
-
-	BEGIN_TRANSITION_MAP							/* Current state */
-		TRANSITION_MAP_ENTRY(ST_SAFETY_SETUP)		/* Pod_Startup */
-		TRANSITION_MAP_ENTRY(EVENT_IGNORED)     /* Safety Setup */
-		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Safe Mode */
-		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Functional test */
-		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Launch ready */
-		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Flight accel */
-		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Flight coast */
-		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Flight brake */
-	END_TRANSITION_MAP(NULL)
-}
 
 void Pod_State::move_functional_tests(){
 	BEGIN_TRANSITION_MAP							/* Current state */
-		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Pod_Startup */
-		TRANSITION_MAP_ENTRY(EVENT_IGNORED)    	/* Safety Setup */
-		TRANSITION_MAP_ENTRY(ST_FUNCTIONAL_TEST)/* Safe Mode */
+		TRANSITION_MAP_ENTRY(ST_FUNCTIONAL_TEST)			/* Safe Mode */
 		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Functional test */
+		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Loading */
 		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Launch ready */
 		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Flight accel */
 		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Flight coast */
@@ -38,23 +25,32 @@ void Pod_State::move_functional_tests(){
 
 void Pod_State::move_safe_mode() {
 	BEGIN_TRANSITION_MAP							/* Current state */
-		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Pod_Startup */
-		TRANSITION_MAP_ENTRY(ST_SAFE_MODE)     	/* Safety Setup */
-		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Safe Mode */
-		TRANSITION_MAP_ENTRY(ST_SAFE_MODE)			/* Functional test */
-		TRANSITION_MAP_ENTRY(ST_SAFE_MODE)			/* Launch ready */
+		TRANSITION_MAP_ENTRY(ST_SAFE_MODE)			/* Safe Mode */
+		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Functional test */
+		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Loading */
+		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Launch ready */
 		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Flight accel */
 		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Flight coast */
 		TRANSITION_MAP_ENTRY(ST_SAFE_MODE)			/* Flight brake */
 	END_TRANSITION_MAP(NULL)
 }
 
+void Pod_State::move_loading() {
+	BEGIN_TRANSITION_MAP							/* Current state */
+		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Safe Mode */
+		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Functional test */
+		TRANSITION_MAP_ENTRY(ST_LOADING)	  		/* Loading */
+		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Launch ready */
+		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Flight accel */
+		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Flight coast */
+		TRANSITION_MAP_ENTRY(EVENT_IGNORED)		/* Flight brake */
+	END_TRANSITION_MAP(NULL)
+}
 void Pod_State::move_launch_ready() {
 	BEGIN_TRANSITION_MAP							/* Current state */
-		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Pod_Startup */
-		TRANSITION_MAP_ENTRY(EVENT_IGNORED)     /* Safety Setup */
 		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Safe Mode */
-		TRANSITION_MAP_ENTRY(ST_LAUNCH_READY)		/* Functional test */
+		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Functional test */
+		TRANSITION_MAP_ENTRY(ST_LAUNCH_READY)		/* Loading */
 		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Launch ready */
 		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Flight accel */
 		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Flight coast */
@@ -65,14 +61,13 @@ void Pod_State::move_launch_ready() {
 // it is important all states should move to braking when this function is called, this is for emergencies
 void Pod_State::emergency_brake() {
 	BEGIN_TRANSITION_MAP							/* Current state */
-		TRANSITION_MAP_ENTRY(ST_FLIGHT_BRAKE)		/* SAFE MODE */
-		TRANSITION_MAP_ENTRY(ST_FLIGHT_BRAKE)		/* FUNCTIONAL TEST */
-		TRANSITION_MAP_ENTRY(ST_FLIGHT_BRAKE)		/* FLIGHT ACCEL */
-		TRANSITION_MAP_ENTRY(ST_FLIGHT_BRAKE)		/* FLIGHT COAST */
-		TRANSITION_MAP_ENTRY(ST_FLIGHT_BRAKE)		/* FLIGHT BRAKE */
-		TRANSITION_MAP_ENTRY(ST_FLIGHT_BRAKE)		/* FLIGHT BRAKE */
-		TRANSITION_MAP_ENTRY(ST_FLIGHT_BRAKE)		/* FLIGHT BRAKE */
-		TRANSITION_MAP_ENTRY(ST_FLIGHT_BRAKE)		/* FLIGHT BRAKE */
+		TRANSITION_MAP_ENTRY(ST_FLIGHT_BRAKE)			/* Safe Mode */
+		TRANSITION_MAP_ENTRY(ST_FLIGHT_BRAKE)			/* Functional test */
+		TRANSITION_MAP_ENTRY(ST_FLIGHT_BRAKE)			/* Loading */
+		TRANSITION_MAP_ENTRY(ST_FLIGHT_BRAKE)			/* Launch ready */
+		TRANSITION_MAP_ENTRY(ST_FLIGHT_BRAKE)			/* Flight accel */
+		TRANSITION_MAP_ENTRY(ST_FLIGHT_BRAKE)			/* Flight coast */
+		TRANSITION_MAP_ENTRY(ST_FLIGHT_BRAKE)			/* Flight brake */
 	END_TRANSITION_MAP(NULL)
 }
 
@@ -82,11 +77,10 @@ void Pod_State::emergency_brake() {
 
 void Pod_State::accelerate() {
 	BEGIN_TRANSITION_MAP							/* Current state */
-		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Pod_Startup */
-		TRANSITION_MAP_ENTRY(EVENT_IGNORED)    	/* Safety Setup */
 		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Safe Mode */
 		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Functional test */
-		TRANSITION_MAP_ENTRY(ST_FLIGHT_ACCEL)		/* Launch ready */
+		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Loading */
+		TRANSITION_MAP_ENTRY(ST_FLIGHT_ACCEL)			/* Launch ready */
 		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Flight accel */
 		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Flight coast */
 		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Flight brake */
@@ -94,12 +88,11 @@ void Pod_State::accelerate() {
 }
 void Pod_State::coast() {
 	BEGIN_TRANSITION_MAP							/* Current state */
-		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Pod_Startup */
-		TRANSITION_MAP_ENTRY(EVENT_IGNORED)    	/* Safety Setup */
 		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Safe Mode */
 		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Functional test */
+		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Loading */
 		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Launch ready */
-		TRANSITION_MAP_ENTRY(ST_FLIGHT_COAST)		/* Flight accel */
+		TRANSITION_MAP_ENTRY(ST_FLIGHT_COAST)			/* Flight accel */
 		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Flight coast */
 		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Flight brake */
 	END_TRANSITION_MAP(NULL)
@@ -107,52 +100,38 @@ void Pod_State::coast() {
 
 void Pod_State::brake() {
 	BEGIN_TRANSITION_MAP							/* Current state */
-		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Pod_Startup */
-		TRANSITION_MAP_ENTRY(EVENT_IGNORED)    	/* Safety Setup */
 		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Safe Mode */
 		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Functional test */
+		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Loading */
 		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Launch ready */
 		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Flight accel */
-		TRANSITION_MAP_ENTRY(ST_FLIGHT_BRAKE)		/* Flight coast */
+		TRANSITION_MAP_ENTRY(ST_FLIGHT_BRAKE)			/* Flight coast */
 		TRANSITION_MAP_ENTRY(EVENT_IGNORED)			/* Flight brake */
 	END_TRANSITION_MAP(NULL)
 }
 
-
-// State Machine State functions
-void Pod_State::ST_Pod_Startup() {
-	std::cout << "Entering: Pod Startup" << std::endl;
-  //Do nothing on transition into this state
-}
-
-void Pod_State::ST_Safety_Setup() {
-	std::cout << "Entering: Safety Setup" << std::endl;
-}
-
 void Pod_State::ST_Safe_Mode() {
-	std::cout << "Entering: Safe Mode" << std::endl;
+  write_sim_command(STATE, get_current_state());
 }
 
 void Pod_State::ST_Functional_Test() {
-	std::cout << "Entering: Functional Test" << std::endl;
- //Do nothing on transition into this state
+  write_sim_command(STATE, get_current_state());
 }
-
+void Pod_State::ST_Loading() {
+  write_sim_command(STATE, get_current_state());
+}
 void Pod_State::ST_Launch_Ready() {
-	std::cout << "Entering: Launch Ready" << std::endl;
-  //Do nothing on transition into this state
+  write_sim_command(STATE, get_current_state());
 }
 
 void Pod_State::ST_Flight_Accel() {
-	std::cout << "Entering: Flight Accel" << std::endl;
-  //Do nothing on transition into this state
+  write_sim_command(STATE, get_current_state());
 }
 
 void Pod_State::ST_Flight_Coast() {
-	std::cout << "Entering: Flight Coast" << std::endl;
-  //Do nothing on transition into this state
+  write_sim_command(STATE, get_current_state());
 }
 
 void Pod_State::ST_Flight_Brake() {
-	std::cout << "Entering: Flight Brake" << std::endl;
+  write_sim_command(STATE, get_current_state());
 }
