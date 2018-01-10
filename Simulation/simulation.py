95252da0d2b91a6224646612b9b91aabb38bbffe
clowder <curtislowder@gmail.com>
2018-01-10 15:19:14 -0600

    updated the way variables are stored

--- Simulation/simulation.py
+++ Simulation/simulation.py
@@ -8,6 +8,19 @@ ts = os.get_terminal_size()
 
 numbars = ts.columns - 40
 
+data = {
+"dist":0.0,
+"track_len":1400.0,
+"vel":0.0,
+"accel":1,
+"deltaT":0.1,
+"epoch": 0.0,
+"drag":0.0,
+"delta_drag":0.0,
+"fric": 0.0,
+"delta_fric":0.0
+}
+'''
 dist = 0.0
 track_len = 1400.0
 vel = 0.0
@@ -18,6 +31,7 @@ drag = 0.0
 delta_drag = 0.0
 fric = 0.0
 delta_fric = 0.0
+'''
 print("Simulation")
 
 def sign(a):
@@ -26,65 +40,66 @@ def sign(a):
 	return -1
 
 def update():#(dist, vel, accel):
-	global dist, vel, accel, deltaT, drag, delta_drag, fric, delta_fric
+	global data
 	# handle drag
 	# accel due to drag
 	
 	# handle kinematics
-	dist = dist + vel*deltaT + ((.5 * accel) * deltaT**2)
-	vel = vel + accel*deltaT
+	data["dist"] = data["dist"] + data["vel"]*data["deltaT"] + ((.5 * data["accel"]) * data["deltaT"]**2)
+	data["vel"] = data["vel"] + data["accel"]*data["deltaT"]
 	
 	# handle drag/fric forces
 	# follows this equation
 	# Fdrag = ((1/2)*C*p*A) * Velocity**2
 	# drag referes to the constant in this equation divided by weight of the pod
-	accel_drag = drag * vel**2
-	if (vel != 0 and abs(vel - sign(vel)*accel_drag*deltaT) > 0):
-		vel = vel - sign(vel)*accel_drag*deltaT
-		drag += delta_drag
+	data["accel_drag"] = data["drag"] * data["vel"]**2
+	if (data["vel"] != 0 and abs(data["vel"] - sign(data["vel"])*data["accel_drag"]*data["deltaT"]) > 0):
+		data["vel"] = data["vel"] - sign(data["vel"])*data["accel_drag"]*data["deltaT"]
+		data["drag"] += data["delta_drag"]
 		pass
 	else:
 		# drag will slow down pod to stop
-		vel = 0
+		data["vel"] = 0
 	# friction force
 	# has equation Ffric = mu * Normal = fric*massOfPod
-	if ( vel != 0 and abs(vel - sign(vel)*fric*deltaT) > 0):
-		vel = vel - sign(vel)*fric*deltaT
-		fric += delta_fric
+	if ( data["vel"] != 0 and abs(data["vel"] - sign(data["vel"])*data["fric"]*data["deltaT"]) > 0):
+		data["vel"] = data["vel"] - sign(data["vel"])*data["fric"]*data["deltaT"]
+		data["fric"] += data["delta_fric"]
 		pass
 	else:
 		# fric will slow down pod to stop
-		vel = 0
+		data["vel"] = 0
 	
-	if (dist > track_len):
-		dist = track_len
-		vel = 0
-		accel = 0
+	if (data["dist"] > data["track_len"]):
+		data["dist"] = data["track_len"]
+		data["vel"] = 0
+		data["accel"] = 0
 
-	return (dist, vel)
+	return (data["dist"], data["vel"])
 
 def executeStatement(statement):
-	global dist, vel, accel, deltaT, drag, delta_drag, fric, delta_fric
+	#global dist, vel, accel, deltaT, drag, delta_drag, fric, delta_fric
+	global data
 	varname = statement[0]
 	value = float(statement[1])
 	
 	if (varname == "a" or varname == "%v"):
-		accel = value
+		data["accel"] = value
 	elif (varname == "v" or varname == "%x"):
-		vel = value
+		data["vel"] = value
 	elif (varname == "x"):
-		dist = value
+		data["dist"] = value
 	elif (varname == "drag"):
-		drag = value
+		data["drag"] = value
 	elif (varname == "%drag"):
-		delta_drag = value
+		data["delta_drag"] = value
 	elif (varname == "fric"):
-		fric = value
+		data["fric"] = value
 	elif (varname == "%fric"):
-		delta_fric = value	
+		data["delta_fric"] = value	
 
 def executeBlock(block):
-	global dist, vel, accel, deltaT, drag, delta_drag
+	global data
 	stat = block['stat']
 	for s in stat:
 		s = s.split('=')
@@ -102,6 +117,7 @@ startTime = 0.0
 nextBlock = 0
 def handleStruct(struct, newTime):
 	global nextBlock
+	global data
 	if (struct is None):
 		return
 	
@@ -115,17 +131,26 @@ def handleStruct(struct, newTime):
 	if (condl[0].strip() == "t"):
 		# check if time has crossed over
 		#print(newTime - float(condl[1]))
-		if ( float(condl[1]) - epoch < 0):
+		if ( float(condl[1]) - data["epoch"] < 0):
 			executeBlock(block)
 			nextBlock += 1
 
 def printStatus():
+	global data
+		
+	vel = data["vel"]
+	accel = data["accel"]
+	dist = data["dist"]
+	drag = data["drag"]
+	fric = data["fric"]
+	track_len = data["track_len"]
+	epoch = data["epoch"]
+
 	drawBars = int(((dist)/track_len)*numbars)
 	bar = "="*drawBars
 	spaces = ""
 	if numbars - drawBars > 0:
 		spaces = "_"*(numbars-drawBars)
-	
 	accel_drag = sign(vel) * drag * vel**2 if abs(vel) >= .001 else 0
 	accel_fric = sign(vel) * fric if abs(vel) >= .001 else 0
 	#sys.stdout.write('%s|%s> <d:%.2f v:%.2f a:%.2f t:%d>\r' % (bar, spaces, dist, vel if abs(vel) >= 0.001 else 0, accel - accel_fric - accel_drag, epoch))
@@ -140,16 +165,18 @@ if __name__ == "__main__":
 	elif len(sys.argv) == 2:
 		struct = parseFromFile(sys.argv[1])
 		#print(json.dumps(struct, indent=4))
+	#epoch = data["epoch"]
 
 	startTime = time.time()
 	printStatus()
 	oldTime = time.time()
-	while epoch < 200:
+	while data["epoch"] < 200:
 		newTime = time.time()
-		handleStruct(struct, epoch)
+		handleStruct(struct, data["epoch"])
 		deltaT = newTime-oldTime
+		data["deltaT"] = deltaT
 		oldTime = time.time()
 		update()
-		epoch = epoch + deltaT
+		data["epoch"] = data["epoch"] + deltaT
 		printStatus()
 	print()
--- Simulation/starter.py
+++ Simulation/starter.py
@@ -1,6 +1,36 @@
 from subprocess import Popen, PIPE
 import sys
+import getopt
 
+def main():
+	argv = sys.argv[1:]
+	try:
+		optlist, args = getopt.getopt(argv, 'p:h')
+	except getopt.GetoptError as err:
+		usage()
+		sys.exit(2)
+	print("optlist", optlist)
+	print("args   ",args)
+
+	if (len(optlist) == 0):
+		usage()
+		sys.exit(0)
+	
+	for o, a in optlist:
+		if o in ("-h","-help"):
+			usage()
+			sys.exit()
+
+def usage():
+	print("SIMULATOR HYPERLOOP 2018™")
+	print("-p <location>: path to the pod executable")
+	print("-h: display usage (this)")
+	print("")
+
+if __name__ == "__main__":
+	main()
+
+'''
 args = sys.argv
 if len(args) == 1:
 	args = ("python3", "simulation.py")
@@ -21,3 +51,4 @@ while True:
 		sys.stdout.write(line)
 
 print("done with starter.py")
+'''
