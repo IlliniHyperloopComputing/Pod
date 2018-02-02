96e4210ab2fb3ed2ca00df47c5d22ca1fe5ae2ec
markschmidt <mwschmi2@illinois.edu>
2018-02-02 13:17:23 -0600

    Queue stuff

---
+++ CentralComputing/SPSCQueue.cpp
@@ -0,0 +1,16 @@
+#include "SPSCQueue.h"
+
+template <class T>
+SPSCQueue<T>::SPSCQueue() {
+  //nothing to do here
+}
+
+template <class T>
+T SPSCQueue<T>::dequeue() {
+  return nullptr;
+}
+
+template <class T>
+void SPSCQueue<T>::enqueue(T object) {
+
+}
