088831c87db47950e34fbbeb77bc0508430c7eb3
Richard Wendel <rgw3d@aol.com>
2018-08-03 20:51:53 -0600

    Untested, but added code to send data to ARM, and updated the timing code to actually be correct.

--- BBBSetup/pru_encoder/main.c
+++ BBBSetup/pru_encoder/main.c
@@ -35,7 +35,6 @@ volatile register uint32_t __R31;
  * Using the name 'rpmsg-pru' will probe the rpmsg_pru driver found
  * at linux-x.y.z/drivers/rpmsg/rpmsg_pru.c
  */
-//#define CHAN_NAME           "rpmsg-client-sample"
 #define CHAN_NAME         "rpmsg-pru"
 
 #define CHAN_DESC           "Channel 31"
@@ -47,8 +46,9 @@ volatile register uint32_t __R31;
  */
 #define VIRTIO_CONFIG_S_DRIVER_OK   4
 
-
 /* Defines */
+
+#define MESSAGE_PERIOD  600000 // 3ms right now
 
 #define ENC0    (1 << 8)  // P8_27
 #define ENC1    (1 << 10) // P8_28
@@ -65,55 +65,35 @@ volatile register uint32_t __R31;
 // array of masks so I can easily loop through them
 uint32_t masks[11] = {ENC0, ENC1, ENC2, ENC3, ENC4, ENC5, ENC6, ENC7, ENC8, ENC9, ENC10};
 
-uint32_t htime_ctr = 0;
 
+// Values to be sent to the ARM
 uint32_t counts[11] = {0}; //Number of times we have seen the signal
+
+uint32_t hdecay[11] = {0}; //upper 32 bits of the last time the signal was high
+uint32_t ldecay[11] = {0}; //lower 32 bits of the last time the signal was high
+
+uint32_t hdelta[11] = {0}; //upper 32 bits of the time between the most recently seen signal and the signal before that one.
+uint32_t ldelta[11] = {0}; //lower 32 bits of the time between the most recently seen signal and the signal before that one.
+
+// Used in debouncing, and to calculate the above
+uint32_t htime_ctr = 0;
 uint32_t htime[11] = {0}; //upper 32 bits of last time there was a high or low signal
 uint32_t ltime[11] = {0}; //lower 32 bits of last time there was a high or low signal
-
-uint32_t hlasthigh[11] = {0};
-uint32_t llasthigh[11] = {0}; // These are what the time deltas are based off of.
-
-uint32_t hdelta[11] = {0}; //time between the most recently seen signal and the signal before that one.
-uint32_t ldelta[11] = {0};
-
 uint8_t isHigh[11] = {0}; //Signal is currently high, need it to go low.
+
 
 uint8_t payload[RPMSG_BUF_SIZE];
 
+char STARTING[9] = "Starting\n";
+char RESETING[9] = "Reseting\n";
+
 #include "optical.h"
 
-void init_rpmsg(void)
-{
-
-
-}
-
 void main(void)
-
 {
 
-    struct pru_rpmsg_transport transport;
-    uint16_t src, dst, len;
-    volatile uint8_t *status;
-
-    /* Allow OCP master port access by the PRU so the PRU can read external memories */
-    CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
-
-    /* Clear the status of the PRU-ICSS system event that the ARM will use to 'kick' us */
-    CT_INTC.SICR_bit.STS_CLR_IDX = FROM_ARM_HOST;
-
-    /* Make sure the Linux drivers are ready for RPMsg communication */
-    status = &resourceTable.rpmsg_vdev.status;
-    while (!(*status & VIRTIO_CONFIG_S_DRIVER_OK));
-
-    /* Initialize the RPMsg transport structure */
-    pru_rpmsg_init(&transport, &resourceTable.rpmsg_vring0, &resourceTable.rpmsg_vring1, TO_ARM_HOST, FROM_ARM_HOST);
-
-    /* Create the RPMsg channel between the PRU and ARM user space using the transport structure. */
-    while (pru_rpmsg_channel(RPMSG_NS_CREATE, &transport, CHAN_NAME, CHAN_DESC, CHAN_PORT) != PRU_RPMSG_SUCCESS);
-
-
+    //GPIO setup
+    /////////////////////////////////////////////
     /* Configure GPI and GPO as Mode 0 (Direct Connect) */
     CT_CFG.GPCFG0 = 0x0000;
     /* Clear GPO pins */
@@ -121,6 +101,9 @@ void main(void)
     /* Clear any pending PRU-generated events */
     __R31 = 0x00000000;
 
+
+    //Timer setup (Ethernet IEP)
+    /////////////////////////////////////////////
     CT_IEP.TMR_GLB_CFG_bit.CNT_EN = 0x0; // disable counter
     CT_IEP.TMR_GLB_CFG_bit.DEFAULT_INC = 0x1; // set increment value to 1
     CT_IEP.TMR_GLB_STS_bit.CNT_OVF = 0x1; // Write 1 to clear overflow
@@ -131,31 +114,101 @@ void main(void)
     CT_IEP.TMR_GLB_CFG_bit.CNT_EN = 0x1; // enable counter
 
 
-    int i, j;
+    // RPMSG Setup
+    /////////////////////////////////////////////
+    struct pru_rpmsg_transport transport;
+    uint16_t src, dst, len;
+    volatile uint8_t *status;
+
+    /* Allow OCP master port access by the PRU so the PRU can read external memories */
+    CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
+    /* Clear the status of the PRU-ICSS system event that the ARM will use to 'kick' us */
+    CT_INTC.SICR_bit.STS_CLR_IDX = FROM_ARM_HOST;
+    /* Make sure the Linux drivers are ready for RPMsg communication */
+    status = &resourceTable.rpmsg_vdev.status;
+    while (!(*status & VIRTIO_CONFIG_S_DRIVER_OK));
+    /* Initialize the RPMsg transport structure */
+    pru_rpmsg_init(&transport, &resourceTable.rpmsg_vring0, &resourceTable.rpmsg_vring1, TO_ARM_HOST, FROM_ARM_HOST);
+    /* Create the RPMsg channel between the PRU and ARM user space using the transport structure. */
+    while (pru_rpmsg_channel(RPMSG_NS_CREATE, &transport, CHAN_NAME, CHAN_DESC, CHAN_PORT) != PRU_RPMSG_SUCCESS);
+
+
+    // Delay doing anything until we receive a start signal
+    /////////////////////////////////////////////
+    while (! (__R31 & HOST_INT));
+    /* Clear the event status */
+    CT_INTC.SICR_bit.STS_CLR_IDX = FROM_ARM_HOST;
+    /* Receive all available messages, multiple messages can be sent per kick */
+    while (pru_rpmsg_receive(&transport, &src, &dst, payload, &len) == PRU_RPMSG_SUCCESS) {
+        /* Write that we are starting */
+        pru_rpmsg_send(&transport, dst, src, STARTING, sizeof(STARTING));
+    }
+
+
+    // Loop Forever
+    // If we receive any message from the host, reset all values
+    /////////////////////////////////////////////
+    uint32_t tic = CT_IEP.TMR_CNT;
+    uint32_t tic_diff = 0;
+    uint8_t send_idx;
+    int i;
     while (1) {
+
         for(i = 0; i<11; i++){
             debounce(masks[i], i);
         }
 
         /* Check bit 30 of register R31 to see if the ARM has kicked us */
-        if (__R31 & HOST_INT) {
+        if (__R31 & HOST_INT){
             /* Clear the event status */
             CT_INTC.SICR_bit.STS_CLR_IDX = FROM_ARM_HOST;
             /* Receive all available messages, multiple messages can be sent per kick */
             while (pru_rpmsg_receive(&transport, &src, &dst, payload, &len) == PRU_RPMSG_SUCCESS) {
-                /* Echo the message back to the same address from which we just received */
-                pru_rpmsg_send(&transport, dst, src, (uint8_t*)&htime_ctr, sizeof(htime_ctr));
+                /* Write that we are reseting */
+                pru_rpmsg_send(&transport, dst, src, RESETING, sizeof(RESETING));
+
+                //Reset all values
+                htime_ctr = 0;
+                for(i=0; i<11; i++){
+                    counts[i] = 0;
+                    hdecay[i] = 0;
+                    ldecay[i] = 0;
+                    hdelta[i] = 0;
+                    ldelta[i] = 0;
+                    htime[i] = 0;
+                    ltime[i] = 0;
+                    isHigh[i] = 0;
+                }
             }
         }
 
-        /*if(j % 100 == 0){
-            len = 3;
-            pru_rpmsg_send(&transport, dst, src, payload, len);
-        }
-        j++;
-        */
-    }
+        // Need to send data to ARM
+        // Can't send all at once
+        // Can send something every 3 ms
 
+        tic_diff = CT_IEP.TMR_CNT - tic;
+        if(tic_diff > MESSAGE_PERIOD){
+
+            //Send what type of data are we sending
+            pru_rpmsg_send(&transport, dst, src, &send_idx, sizeof(send_idx));
+
+            //Send the rest of the data
+            switch (send_idx){
+                case 0:
+                    pru_rpmsg_send(&transport, dst, src, counts, sizeof(counts));
+                    send_idx ++;
+                    break;
+                case 1:
+                    pru_rpmsg_send(&transport, dst, src, ldecay, sizeof(ldecay));
+                    send_idx ++;
+                    break;
+                case 2:
+                    pru_rpmsg_send(&transport, dst, src, ldelta, sizeof(ldelta));
+                    send_idx = 0;
+                    break;
+            }
+        }
+    }
 }
 
 
--- BBBSetup/pru_encoder/optical.h
+++ BBBSetup/pru_encoder/optical.h
@@ -8,6 +8,8 @@
 #ifndef OPTICAL_H_
 #define OPTICAL_H_
 
+#define TWO_THOUSAND_HZ 100000
+
 
 extern uint32_t htime_ctr;
 
@@ -15,8 +17,8 @@ extern uint32_t counts[11]; //Number of times we have seen the signal
 extern uint32_t htime[11]; //upper 32 bits of last time there was a high or low signal
 extern uint32_t ltime[11]; //lower 32 bits of last time there was a high or low signal
 
-extern uint32_t hlasthigh[11];
-extern uint32_t llasthigh[11]; // These are what the time deltas are based off of.
+extern uint32_t hdecay[11];
+extern uint32_t ldecay[11]; // These are what the time deltas are based off of.
 
 extern uint32_t hdelta[11]; //time between the most recently seen signal and the signal before that one.
 extern uint32_t ldelta[11];
@@ -25,7 +27,9 @@ extern uint8_t isHigh[11]; //Signal is currently high, need it to go low.
 
 uint32_t htime_diff = 0;
 uint32_t ltime_diff = 0;
+uint32_t cur_time = 0;
 
+/*
 #define diff_time(htime_diff, ltime_diff, htime, ltime) {\
     htime_diff = htime_ctr - htime;\
     ltime_diff = CT_IEP.TMR_CNT - ltime;\
@@ -37,40 +41,66 @@ uint32_t ltime_diff = 0;
         htime_ctr ++;\
     }\
 }
+*/
+
+//htime_ctr - htime > 1  then set max
+//htime_ctr - htime == 1 and curtime >
+//#define diff_time_new(htime_diff, ltime_diff, htime, ltime) {\
+
+//}
+
 
 
 inline
 void debounce(const uint32_t mask, const uint32_t i) {
+
+    // Grab the current time
+    cur_time = CT_IEP.TMR_CNT;
+    hdecay[i] = htime_ctr - htime[i];
+
+    // Check if there was an overflow
+    if(CT_IEP.TMR_GLB_STS_bit.CNT_OVF != 0){
+            CT_IEP.TMR_GLB_STS_bit.CNT_OVF = 1;
+            hdecay[i] ++;
+            htime_ctr ++;
+    }
+
+    // Set the decay
+    if(hdecay[i] > 1){
+        ldecay[i] = UINT32_MAX;
+    }
+    else if(hdecay[i] == 1 && cur_time > ltime[i] ){
+        ldecay[i] = UINT32_MAX;
+    }
+    else{
+        ldecay[i] = cur_time - ltime[i];
+    }
+
+
+    // Do the debounce update
     if(!isHigh[i]){
-        diff_time(htime_diff, ltime_diff, htime[i], ltime[i]);
-        if((__R31 & mask) == mask){
-
-            diff_time(htime_diff, ltime_diff, htime[i], ltime[i]);
-
-            if(htime_diff > 0 || ltime_diff > 100000){
+        if((__R31 & mask) == mask){ // signal is high
+            if(ltime_diff > TWO_THOUSAND_HZ){ // debounce
                 isHigh[i] = 1;
                 counts[i]++;
 
-                htime[i] += htime_diff;
-                ltime[i] += ltime_diff;
+                hdelta[i] = hdecay[i];
+                ldelta[i] = ldecay[i];
 
-                diff_time(hdelta[i], ldelta[i], hlasthigh[i], llasthigh[i]);
 
-                hlasthigh[i] = htime[i];
-                llasthigh[i] = ltime[i];
+                htime[i] = htime_ctr;
+                ltime[i] = CT_IEP.TMR_CNT;
             }
         }
     }
     else{
-        diff_time(htime_diff, ltime_diff, htime[i], ltime[i]);
-        if((__R31 & mask) != mask){
-            diff_time(htime_diff, ltime_diff, htime[i], ltime[i]);
 
-            if(htime_diff > 0 || ltime_diff > 100000){
+        if((__R31 & mask) != mask){ // signal is low
+            if(ltime_diff > TWO_THOUSAND_HZ){ // debounce
                 isHigh[i]=0;
 
-                htime[i] += htime_diff;
-                ltime[i] += ltime_diff;
+                htime[i] = htime_ctr;
+                ltime[i] = CT_IEP.TMR_CNT;;
             }
         }
     }
