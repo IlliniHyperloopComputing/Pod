1fad75b065a95565637f63550f5749044f7646cb
Richard Wendel <rgw3d@aol.com>
2018-08-05 13:27:11 -0600

    PRUManager is up! Along with an implementation of Velocity, with some basic filtering.

--- CentralComputing/PRUManager.hpp
+++ CentralComputing/PRUManager.hpp
@@ -2,19 +2,29 @@
 #define PRU_MANAGER_HPP
 
 #include "SourceManagerBase.hpp"
+#include <sys/poll.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define DEVICE_NAME		"/dev/rpmsg_pru31"
+#define MAX_BUFFER_SIZE		512
 
 struct PRUData {
-  //replace with actual data structure
-  int dummy_data;
+	uint32_t counts[11];
+	uint32_t decays[11];
+	uint32_t deltas[11];
 };
 
-class PRUManager : public SourceManagerBase<(long long) (1.0 * 1E6), PRUData> {
+class PRUManager : public SourceManagerBase<(long long) (0.010 * 1E6), PRUData> {
   private:
     std::shared_ptr<PRUData> refresh();
-      
 
-    //iterator for testing purposes, remove
-    int i = 5;
+    bool initalized;
+    bool failed;
+    uint8_t readBuf[MAX_BUFFER_SIZE];
+	  struct pollfd pollfds[1];
+      
 };
 
 #endif
--- CentralComputing/Velocity.hpp
+++ CentralComputing/Velocity.hpp
@@ -2,14 +2,53 @@
 #define VELOCITY_H
 
 #include "PodParameter.hpp"
+#include "Filter.h"
+#include <algorithm>
+
+#define NUM_VEL_INPUTS 4
 
 class Velocity : public PodParameter<double> {
   public:
+    
+    int    velocity_idx[NUM_VEL_INPUTS] = {1, 3, 5, 7}; 
+    double velocities[NUM_VEL_INPUTS] = {0};
+    double velocity_output = 0;
+
     shared_ptr<double> Get() {
-      // Pull most up to date data from SourceManager
+      // Create variable we are going to return
       shared_ptr<double> p = make_shared<double>(); 
-      //replace with actual variables from PRUManager/OpticalManager
-      *p = SourceManager::PRU.Get()->dummy_data;
+
+      // Grab decays and deltas from PRU
+      auto pru_data = SourceManager::PRU.Get();
+      uint32_t * decays = pru_data->decays;
+      uint32_t * deltas = pru_data->deltas;
+
+      // Strategy: For the sensors we are interested in, use
+      //           the sensor data that provides the lowest
+      //           velocity. Low pass filter. Then take median
+      
+      for(int i = 0; i < NUM_VEL_INPUTS; i++){
+        uint32_t decay = decays[velocity_idx[i]];   
+        uint32_t delta = deltas[velocity_idx[i]];   
+
+        double vel = std::max(decay, delta) * (21.474836475 / 4294967295.0);
+
+        velocities[i] = Filter::LowPass(velocities[i], vel);
+      }
+
+      // Create temporary array
+      double tmp_v[NUM_VEL_INPUTS];
+      memcpy(tmp_v, velocities, sizeof(tmp_v));
+
+      // Find the median value, array will be modified
+      std::nth_element(tmp_v, tmp_v + NUM_VEL_INPUTS/2, tmp_v + NUM_VEL_INPUTS);
+      double median = tmp_v[NUM_VEL_INPUTS/2];
+      
+      // LowPass filter once more
+      velocity_output = Filter::LowPass(velocity_output, median);
+
+      // Set the return value
+      *p = velocity_output;
       return p;
     }
 };
