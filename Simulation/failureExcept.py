baaaeed4bc77edd2655167f5fa1b97783dcebc56
Richard Wendel <rgw3d@aol.com>
2018-08-04 18:22:29 -0600

    Sins of poor git management. Removing more unecessary files.

--- Simulation/failureExcept.py
+++
@@ -1,6 +0,0 @@
-# Custom exception used for when a failure flag is run
-class FailureException(Exception):
-	def __init__(self, value):
-		self.parameter = value
-	def __str__(self):
-		return repr(self.parameter)
