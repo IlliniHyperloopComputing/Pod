7f99967b294fea70f68b274ebc486ecaa6ddfc78
Richard Wendel <rgw3d@aol.com>
2018-08-06 22:58:39 -0600

    I added COLOR to some of the print out statements.

--- CentralComputing/Utils.cpp
+++ CentralComputing/Utils.cpp
@@ -3,13 +3,35 @@
 #include <unistd.h>
 #include <errno.h>
 
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
 Utils::LogLevel Utils::loglevel = LOG_ERROR;
 
 void Utils::print(LogLevel level, const char * format, ...){
       if(loglevel <= level){
+        char buffer[256];
+        if(level == LOG_ERROR){
+          snprintf(buffer, 256, "%s[ERROR]:%s %s", ANSI_COLOR_RED, ANSI_COLOR_RESET, format);
+        }
+        else if(level == LOG_INFO){
+          snprintf(buffer, 256, "%s[INFO ]:%s %s", ANSI_COLOR_GREEN, ANSI_COLOR_RESET, format);
+        }
+        else if(level == LOG_INFO){
+          snprintf(buffer, 256, "%s[DEBUG]:%s %s", ANSI_COLOR_YELLOW, ANSI_COLOR_RESET, format);
+        }
+        else if(level == LOG_EDEBUG){
+          snprintf(buffer, 256, "%s", format);
+        }
+
         va_list args;
         va_start(args, format);
-        vfprintf(stdout, format, args);
+        vfprintf(stdout, buffer, args);
         va_end(args);
       }
 }
