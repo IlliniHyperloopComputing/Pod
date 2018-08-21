fc3a765a9977cc62ad3c2ebe8a35ed9ac0496358
Mark Schmidt <mwschmi2@illinois.edu>
2018-08-21 19:18:18 +0000

    I love git blame

--- CentralComputing/ParameterManager.cpp
+++ CentralComputing/ParameterManager.cpp
@@ -8,9 +8,9 @@ vector<uint8_t> ParameterManager::get_network_packet() {
   //Is there a better way to do this? Probably, but I'm lazy
   
   //This is how you send the packet information for a single parameter
-  //bytes.push_back(NetworkManager::Network_Data_ID::VELOCITY);
-  //auto v_bytes = velocity.get_network_packet();
-  //bytes.insert(bytes.end(), v_bytes.begin(), v_bytes.end());
+  bytes.push_back(NetworkManager::Network_Data_ID::TEMPERATURE);
+  auto t_bytes = temperature.get_network_packet();
+  bytes.insert(bytes.end(), t_bytes.begin(), t_bytes.end());
  
   return bytes;
 }
