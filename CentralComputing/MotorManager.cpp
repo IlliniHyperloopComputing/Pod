9d0a6d0650ca736a3e636262e4da19bddd13a40f
Richard Wendel <rgw3d@aol.com>
2018-08-21 13:38:57 -0400

    Working on MotorManager PI loop. Going to add test cases next

--- CentralComputing/MotorManager.cpp
+++ CentralComputing/MotorManager.cpp
@@ -1,35 +1,51 @@
 #include "MotorManager.hpp"
 using namespace Utils;
+
 void MotorManager::enable_motors() {
   integral = 0;
-  print(LogLevel::LOG_DEBUG, "Enabling motors\n");
+  enabled = true;
+  set_throttle(MOTOR_OFF);
+  print(LogLevel::LOG_DEBUG, "Motors Enabled\n");
 }
 
 void MotorManager::disable_motors() {
+  set_throttle(MOTOR_OFF);
   integral = 0;
-  print(LogLevel::LOG_DEBUG, "Disabling motors\n");
+  enabled = false;
+
+  print(LogLevel::LOG_DEBUG, "Motors Disabled\n");
 }
 
-uint16_t MotorManager::calculate_throttle(double dt, uint16_t last_throttle){
+int16_t MotorManager::calculate_throttle(double dt, int16_t last_throttle){
   // Get velocity
   // And current disk RPM
   std::shared_ptr<StateSpace> stateSpace = SourceManager::MM.Get();
   double velocity = stateSpace.get()->x[1];
   double rpm      = stateSpace.get()->rpm;
 
+  // Calculate setpoint
   double setPoint = velocity * MS_TO_RPM + RPM_PDR;
 
+  // Calculate error
   double error = setPoint-rpm;
+  // Calculate integral term
   integral += (error * dt);
 
+
+  //TODO: Set limit on dt ?
+  //TODO: Clamp integral term ? 
+
+  // Calculate PI output
   int output = (int) (RPM_TO_USEC * ((KP * error) + (KI * integral)));
 
-  output = Utils::clamp(output, -2, 2);
+  // Clamp output to around the last_throttle
+  output = last_throttle + Utils::clamp(output - last_throttle, -2, 2);
   
-  return (uint16_t)Utils::clamp(last_throttle + output, 0, 1000);
-
+  return (int16_t) Utils::clamp(output, 0, 1000);
 }
 
-void MotorManager::set_throttle(uint16_t value) {
-  print(LogLevel::LOG_DEBUG, "Setting motor throttle: %d\n", value);
+void MotorManager::set_throttle(int16_t value) {
+  if(enabled){
+    print(LogLevel::LOG_DEBUG, "Setting motor throttle: %d\n", value);
+  }
 }
--- CentralComputing/MotorManager.hpp
+++ CentralComputing/MotorManager.hpp
@@ -6,11 +6,15 @@
 #include "MotionModel.hpp"
 #include <cmath>
 
+//TODO: Better define these constants
 #define RPM_PDR 600.0  //point of diminishing returns
-#define DISK_CIRCUMFRENCE 10.0
-#define RPM_TO_MS (DISK_CIRCUMFRENCE/60.0)
-#define MS_TO_RPM (60.0/DISK_CIRCUMFRENCE)
-#define RPM_TO_USEC (25.0/400.0)
+#define DISK_CIRCUMFRENCE 10.0 //In Meters
+#define RPM_TO_MS (DISK_CIRCUMFRENCE/60.0) // RPM to Meters per second
+#define MS_TO_RPM (60.0/DISK_CIRCUMFRENCE) // Meters per second to RPM
+//TODO: Better define this constant
+#define RPM_TO_USEC (25.0/400.0) // Usec/RRPM
+
+#define MOTOR_OFF -50.0
 
 #define KP 1.0
 #define KD 0.0
@@ -32,16 +36,17 @@ class MotorManager {
     /*
      * Uses a PI loop to calculate the appropriate motor throttle
      */
-    uint16_t calculate_throttle(double dt, uint16_t last_throttle);
+    int16_t calculate_throttle(double dt, int16_t last_throttle);
 
     /*
      * Sets the motor throttle to a specific value
      */
-    void set_throttle(uint16_t value);
+    void set_throttle(int16_t value);
 
   private:
     double integral;
 
+    bool enabled;
 
 };
 
