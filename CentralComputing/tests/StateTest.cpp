cbdc3485edc7e8047b42c130167a9a9305211a81
root <root@localhost.localdomain>
2018-09-20 04:32:11 -0400

    Welp I tried

--- CentralComputing/tests/StateTest.cpp
+++ CentralComputing/tests/StateTest.cpp
@@ -1,7 +1,11 @@
 #include "PodTest.cpp"
 
 
-TEST_F(PodTest, ConnectDisconnect) {
+TEST_F(PodTest, NonTransitionalTest) {
+  MoveState(NetworkManager::Network_Command_ID::TRANS_FUNCTIONAL_TEST, Pod_State::E_States::ST_FUNCTIONAL_TEST, true);
+  MoveState(NetworkManager::Network_Command_ID::TRANS_LOADING, Pod_State::E_States::ST_LOADING, true);
+  MoveState(NetworkManager::Network_Command_ID::DISABLE_MOTOR, Pod_State::E_States::ST_LOADING, true);
+  MoveState(NetworkManager::Network_Command_ID::DEACTIVATE_BRAKE_MAGNET, Pod_State::E_States::ST_LOADING, true);
 }
 
 
@@ -13,5 +17,26 @@ TEST_F(PodTest, FunctionalTest) {
 TEST_F(PodTest, Loading) {
   MoveState(NetworkManager::Network_Command_ID::TRANS_FUNCTIONAL_TEST, Pod_State::E_States::ST_FUNCTIONAL_TEST, true);
   MoveState(NetworkManager::Network_Command_ID::TRANS_LOADING, Pod_State::E_States::ST_LOADING, true);
+}
+
+TEST_F(PodTest, EmergencyBrake) {
+  MoveState(NetworkManager::Network_Command_ID::TRANS_FUNCTIONAL_TEST, Pod_State::E_States::ST_FUNCTIONAL_TEST, true);
+  MoveState(NetworkManager::Network_Command_ID::TRANS_LOADING, Pod_State::E_States::ST_LOADING, true);
+  MoveState(NetworkManager::Network_Command_ID::TRANS_LAUNCH_READY, Pod_State::E_States::ST_LAUNCH_READY, true);
+  MoveState(NetworkManager::Network_Command_ID::LAUNCH, Pod_State::E_States::ST_FLIGHT_ACCEL, true);
+  MoveState(NetworkManager::Network_Command_ID::EMERGENCY_BRAKE Pod_State::E_States::ST_FLIGHT_BRAKE, true);
+
+}
+
+//Not sure about this but probably cant launch if the motor is turned off and so are the brakes
+
+EST_F(PodTest, NonTransitionalTest) {
+  MoveState(NetworkManager::Network_Command_ID::TRANS_FUNCTIONAL_TEST, Pod_State::E_States::ST_FUNCTIONAL_TEST, true);
+  MoveState(NetworkManager::Network_Command_ID::TRANS_LOADING, Pod_State::E_States::ST_LOADING, true);
+  MoveState(NetworkManager::Network_Command_ID::DISABLE_MOTOR, Pod_State::E_States::ST_LOADING, true);
+  MoveState(NetworkManager::Network_Command_ID::DEACTIVATE_BRAKE_MAGNET, Pod_State::E_States::ST_LOADING, true);
+  MoveState(NetworkManager::Network_Command_ID::TRANS_LOADING, Pod_State::E_States::ST_LOADING, true);
+  MoveState(NetworkManager::Network_Command_ID::TRANS_LAUNCH_READY, Pod_State::E_States::ST_LAUNCH_READY, false);
+  MoveState(NetworkManager::Network_Command_ID::LAUNCH, Pod_State::E_States::ST_FLIGHT_ACCEL, false);
 }
 
