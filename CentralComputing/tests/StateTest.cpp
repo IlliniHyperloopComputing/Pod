123ba17bdc06310fb644b8751b5c1a16a35cdeee
Teja Gupta <tejag2@illinois.edu>
2018-09-21 01:10:43 +0000

    Added some tests

--- CentralComputing/tests/StateTest.cpp
+++ CentralComputing/tests/StateTest.cpp
@@ -15,3 +15,17 @@ TEST_F(PodTest, Loading) {
   MoveState(NetworkManager::Network_Command_ID::TRANS_LOADING, Pod_State::E_States::ST_LOADING, true);
 }
 
+TEST_F(PodTest, FlightAccel){
+MoveState(NetworkManager::Network_Command_ID::TRANS_FUNCTIONAL_TEST,Pod_State::E_States::ST_FUNCTIONAL_TEST,true);
+MoveState(NetworkManager::Network_Command_ID::TRANS_FLIGHT_ACCEL,Pod_State::E_States::ST_FLIGHT_ACCEL,false);
+}
+
+TEST_F(PodTest,FlightCoast){
+MoveState(NetworkManager::Network_Command_ID::TRANS_FUNCTIONAL_TEST,Pod_State::E_States::ST_FUNCTIONAL_TEST,true);
+MoveState(NetworkManager::Network_Command_ID::TRANS_FLIGHT_COAST,Pod_State::E_States::ST_FLIGHT_COAST,false);
+}
+
+TEST_F(PodTest,FlightBrake){
+MoveState(NetworkManager::Network_Command_ID::TRANS_FUNCTIONAL_TEST,Pod_State::E_States::ST_FUNCTIONAL_TEST,true);
+MoveState(NetworkManager::Network_Command_ID::TRANS_FLIGHT_BRAKE,Pod_State::E_States::ST_FLIGHT_BRAKE,false);
+}
