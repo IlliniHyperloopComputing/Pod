6bd60ded4f1ec56443473f648ab6ccadbc98c469
anshulshah957 <42949061+anshulshah957@users.noreply.github.com>
2018-09-20 21:00:17 -0500

    Changed true to false for moving from safe mode to loading

--- CentralComputing/tests/StateTest.cpp
+++ CentralComputing/tests/StateTest.cpp
@@ -5,7 +5,7 @@ TEST_F(PodTest, ConnectDisconnect) {
 
 TEST_F(PodTest, SafeMode) {
   MoveState(NetworkManager::Network_Command_ID::TRANS_SAFE_MODE, Pod_State::E_States::ST_SAFE_MODE, true);
-  MoveState(NetworkManager::Network_Command_ID::TRANS_LOADING, Pod_State::E_States::ST_LOADING, true);
+  MoveState(NetworkManager::Network_Command_ID::TRANS_LOADING, Pod_State::E_States::ST_LOADING, false);
   MoveState(NetworkManager::Network_Command_ID::TRANS_LAUNCH_READY, Pod_State::E_States::ST_LAUNCH_READY, false);
   MoveState(NetworkManager::Network_Command_ID::LAUNCH, Pod_State::E_States::ST_FLIGHT_ACCEL, false);
 }
