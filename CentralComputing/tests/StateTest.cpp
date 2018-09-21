db015f051a1cd07ead5fe0de9f7d26369c56b759
Nagarjun Kumar <nagarjun.kumar@gmail.com>
2018-09-21 00:37:56 +0000

    Updated StateTest

--- CentralComputing/tests/StateTest.cpp
+++ CentralComputing/tests/StateTest.cpp
@@ -2,16 +2,35 @@
 
 
 TEST_F(PodTest, ConnectDisconnect) {
+	MoveState(NetworkManager::Network_Command_ID::TRANS_LOADING,Pod_State::E_States::ST_LOADING,true);
+	MoveState(NetworkManager::Network_Command_ID::TRANS_CONNECT_DISCONNECT,Pod_State::E_States::ST_CONNECT_DISCONNECT,true);
+	MoveState(NetworkManager::Network_Command_ID::TRANS_SAFE_MODE, Pod_State::E_States::ST_SAFE_MODE, true);
+	MoveState(NetworkManager::Network_Command_ID::TRANS_FUNCTIONAL_TEST, Pod_State::E_States::ST_FUNCTIONAL_TEST, true);
 }
 
 
 TEST_F(PodTest, FunctionalTest) {
-  MoveState(NetworkManager::Network_Command_ID::TRANS_FUNCTIONAL_TEST, Pod_State::E_States::ST_FUNCTIONAL_TEST, true);
-  MoveState(NetworkManager::Network_Command_ID::TRANS_LAUNCH_READY, Pod_State::E_States::ST_LAUNCH_READY, false); //this transition should fail because we cannot skip loading
+  	MoveState(NetworkManager::Network_Command_ID::TRANS_FUNCTIONAL_TEST, Pod_State::E_States::ST_FUNCTIONAL_TEST, true);
+  	MoveState(NetworkManager::Network_Command_ID::TRANS_LAUNCH_READY, Pod_State::E_States::ST_LAUNCH_READY, false); 
+  	MoveState(NetworkManager:Network_Command_ID::TRANS_CONNECT_DISCONNECT, Pod_State::E_States::ST_CONNECT_DISCONNECT,true);//this transition should fail because we cannot skip loading
+  	MoveState(NetworkManager::Network_Command_ID::TRANS_SAFE_MODE, Pod_State::E_States::ST_SAFE_MODE, true);
 }
 
 TEST_F(PodTest, Loading) {
-  MoveState(NetworkManager::Network_Command_ID::TRANS_FUNCTIONAL_TEST, Pod_State::E_States::ST_FUNCTIONAL_TEST, true);
-  MoveState(NetworkManager::Network_Command_ID::TRANS_LOADING, Pod_State::E_States::ST_LOADING, true);
+  	MoveState(NetworkManager::Network_Command_ID::TRANS_FUNCTIONAL_TEST, Pod_State::E_States::ST_FUNCTIONAL_TEST, true);
+  	MoveState(NetworkManager::Network_Command_ID::TRANS_LOADING, Pod_State::E_States::ST_LOADING, true);
+  	MoveState(NetworkManager:Network_Command_ID::TRANS_CONNECT_DISCONNECT, Pod_State::E_States::ST_CONNECT_DISCONNECT,true);
+  	MoveState(NetworkManager::Network_Command_ID::TRANS_SAFE_MODE, Pod_State::E_States::ST_SAFE_MODE, true);
 }
-
+TEST_F(PodTest,LaunchReady){
+	MoveState(NetworkManager:Network_Command_ID::TRANS_LAUNCH_READY, Pod_State::E_States::ST_LAUNCHREADY,true);
+	MoveState(NetworkManager:Network_Command_ID::TRANS_CONNECT_DISCONNECT, Pod_State::E_States::ST_CONNECT_DISCONNECT,true);
+	MoveState(NetworkManager::Network_Command_ID::TRANS_FUNCTIONAL_TEST, Pod_State::E_States::ST_FUNCTIONAL_TEST, true);
+	MoveState(NetworkManager::Network_Command_ID::TRANS_SAFE_MODE, Pod_State::E_States::ST_SAFE_MODE, true);
+}
+TEST_F(PodTest,SafeMode){
+	MoveState(NetworkManager:Network_Command_ID::TRANS_LAUNCH_READY, Pod_State::E_States::ST_LAUNCHREADY,true);
+	MoveState(NetworkManager:Network_Command_ID::TRANS_CONNECT_DISCONNECT, Pod_State::E_States::ST_CONNECT_DISCONNECT,true);
+	MoveState(NetworkManager::Network_Command_ID::TRANS_FUNCTIONAL_TEST, Pod_State::E_States::ST_FUNCTIONAL_TEST, true);
+	MoveState(NetworkManager::Network_Command_ID::TRANS_SAFE_MODE, Pod_State::E_States::ST_SAFE_MODE, true);
+}
