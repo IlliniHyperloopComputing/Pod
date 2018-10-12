ec5bc03658a56d1f8d9a7c91f98f4e713dc272d6
Rishi Wadhwa <rishiw2@illinois.edu>
2018-10-12 01:00:19 +0000

    created skeletons of brake.hpp and brake.cpp

--- CentralComputing/Brake.cpp
+++ CentralComputing/Brake.cpp
@@ -2,16 +2,24 @@
 
 Brake::Brake(){}
 
-void Brake::enable_brakes() {}
-
-void Brake::disable_brakes() {}
-
-void Brake::set_enable(){}
-
-void Brake::is_enabled(){
+void Brake::enable_brakes() {
 }
 
-void Brake::calculate_brakeForce(){
+void Brake::disable_brakes() {
 }
 
-void Brake::set_brakeForce() {}
+void Brake::set_enable(bool enable){
+
+}
+
+bool Brake::is_enabled(){
+  return false;
+}
+
+int16_t Brake::calculate_brakeForce(double dt, int16_t last_brakeForce){
+  return int16_t(0);
+}
+
+void Brake::set_brakeForce(int16_t value) {
+
+}
--- CentralComputing/Brake.hpp
+++ CentralComputing/Brake.hpp
@@ -1,26 +1,58 @@
 #ifndef BRAKE_HPP
 #define BRAKE_HPP
 
+#include "Utils.h"
+#include "SourceManager.hpp"
+#include "MotionModel.hpp"
+#include <cmath>
+
 class Brake {
   public:
 
+    /*
+     * Constructor
+     * Setup the PWM pins
+     */
     Brake();
 
+    /*
+     * Arms the brakes
+     */
     void enable_brakes();
 
+    /*
+     * Disarms the brakes
+     */
     void disable_brakes();
 
-    void is_enabled();
+    /*
+     * is_enabled returns enabled variable
+     */
+    bool is_enabled();
 
-    void calculate_brakeForce();
+    /*
+     * Uses a <?> to calculate the appropriate brake force
+     */
+    int16_t calculate_brakeForce(double dt, int16_t last_brakeForce);
 
-    void set_brakeForce();
+    /*
+     * Sets the brake force to a specific value
+     */
+    void set_brakeForce(int16_t value);
 
-    void debug_set_pwm_pin();
+    void debug_set_pwm_pin(std::string & path);
 
   private:
 
-    void set_enable();
+    void set_enable(bool enable);
+
+    static std::string pwm_chip;
+    static std::string pwm_pin;
+
+    double integral;
+
+    bool enabled;
+
 };
 
 #endif
