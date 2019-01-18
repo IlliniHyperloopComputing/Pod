92533d970f063e392fadfa8c827e255cfbe954da
Evan <epaglier@purdue.edu>
2019-01-18 18:20:29 -0500

    Reset the React frontend

--- BaseStation/frontend/src/App.js
+++ BaseStation/frontend/src/App.js
@@ -1,46 +1,28 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
 
-//https://medium.freecodecamp.org/force-refreshing-a-react-child-component-the-easy-way-6cdbb9e6d99c
-
 class App extends Component {
-  state = {
-    "body": {},
-  };
-
-  componentDidMount() {
-    this.intervalID = setInterval(
-      () => this.tick(),
-      100
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
     );
   }
-  componentWillUnmount() {
-    clearInterval(this.intervalID);
-  }
-  tick() {
-    this.callApi()
-      .then(res => this.setState({ body: res }))
-      .catch(err => console.log(err));
-  }
-
-  callApi = async () => {
-    const response = await fetch('/api/complete');
-    const body = await response.json();
-    if (response.status !== 200) throw Error(body.message);
-    return body;
-  };
-
-render() {
-  const {vals} = this.state.body;  // Essentially does: const vals = this.state.vals;
-  return (
-    <div>
-      {
-        Object.keys(vals).map((key, index) => ( 
-          <p key={index}> this is my key {key} and this is my value {vals[key]}</p> 
-        ))
-      }
-    </div>
-  )
-  }
 }
+
 export default App;
