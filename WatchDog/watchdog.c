71f8ad4a914c9732499e4613b5da0a2c67b18350
AFederici <alexanderfederici@hotmail.com>
2019-02-13 23:05:26 +0000

    compiles on arduino

--- WatchDog/watchdog.c
+++ WatchDog/watchdog.c
@@ -1,7 +1,7 @@
-nt inPin = 7; //whatever the input pin is
+int inPin = 7; //whatever the input pin is
 int outPin = 13; //output to tell other hardware if software failed
-int state = 0;   //output value, start LOW
-int lastState = 0; //to check for changing vals
+int heartBeat = 0;   //output value, start LOW
+int lastBeat = 0; //to check for changing vals
 int changeCounter = 0; //just looking for changes
 int totalCounter = 0; //to check if we are changing fast enough
 int mainLoop = 0; //turn on once we know program is running
@@ -12,51 +12,57 @@ int mainLoop = 0; //turn on once we know program is running
 //no idea what these should be
 int waitPeriod = 15;
 int goal = 10; 
-
-
-void setup(){
-pinMode(outPin, OUTPUT);
-pinMode(inPin, INPUT);
+int state = 0;
+  
+  
+void setup() {
+  pinMode(outPin, OUTPUT);
+  pinMode(inPin, INPUT);
+  //START LOW
+  digitalWrite(outPin, 0);
 }
 
-void loop(){
-
-//might want to add some tolerance here
-if (mainLoop){
-	//HEALTHY
-	digitalWrite(outPin, 1);
-	state = digitalRead(inPin);
-	if (state == lastState){
-		//it broke
-		while (1){
-			digitalWrite(outPin, 0);
-		}
-	}
-
-	lastState = state;
+void loop() {
+  switch(state){
+    
+    //NOT STARTED YET
+    case 0:
+      //check for change in hearbeat
+      heartBeat = digitalRead(inPin);
+      if (heartBeat != lastBeat){ changeCounter++; }
+      totalCounter++;
+      lastBeat = heartBeat;
+    
+      //if we get 10 changes in a span of 15 sec continue to healthy state
+      if (changeCounter > goal){
+        state = 1;
+      }
+      
+      //reset counters to continue waiting
+      if (totalCounter > waitPeriod){
+        totalCounter = 0;
+        changeCounter = 0;
+      }
+      break;
+      
+    //HEALTHY STATE
+    case 1:
+      //might want to add some tolerance here
+      digitalWrite(outPin, 1);
+      heartBeat = digitalRead(inPin);
+      if (heartBeat == lastBeat){
+        state = 2;
+      }
+    
+      lastBeat = heartBeat;
+      break;
+      
+    //DEAD STATE
+    case 2:
+        digitalWrite(outPin, 0);
+        while (1){ 
+          //DO NOTHING 
+        }
+  }
 }
 
-else{
-	//reset period
-	if (totalCounter > waitPeriod){
-		totalCounter = 0;
-		changeCounter = 0;
-	}
-
-	//check for change state
-	state = digitalRead(inPin);
-	if (state != lastState){
-		changeCounter++;
-	}
-	totalCounter++;
-	lastState = state;
-
-	//START LOW
-	digitalWrite(outPin, 0);
-
-	//if we get 10 changes in a span of 15 sec continue to main
-	if (changeCounter > goal){
-		mainLoop = 1;
-	}
-}
-}
