c94e487fcaa34d4210b9f6024316ccc8427aba46
AFederici <alexanderfederici@hotmail.com>
2019-02-14 03:11:52 +0000

    tested code works

--- Watchdog/Watchdog.ino
+++ Watchdog/Watchdog.ino
@@ -1,17 +1,17 @@
-int inPin = 7; //whatever the input pin is
-int outPin = 13; //output to tell other hardware if software failed
+int inPin = 1; //whatever the input pin is
+int outPin = 0; //output to tell other hardware if software failed
 int heartBeat = 0;   //output value, start LOW
 int lastBeat = 0; //to check for changing vals
-int changeCounter = 0; //just looking for changes
-int totalCounter = 0; //to check if we are changing fast enough
-int mainLoop = 0; //turn on once we know program is running
 
 //wait for X amount of signal changes (heart beats) within a short 
 //time period (to make sure power cycles or other transients doesn't 
 //accidentally turn it on)
 //no idea what these should be
-int waitPeriod = 15;
-int goal = 10; 
+int changeCounter = 0; //just looking for changes
+int waitPeriod = 260;
+int startTime = 0;
+int currTime = 0;
+int goal = 5; 
 int state = 0;
   
   
@@ -27,34 +27,48 @@ void loop() {
     
     //NOT STARTED YET
     case 0:
+      currTime = millis();
       //check for change in hearbeat
       heartBeat = digitalRead(inPin);
       if (heartBeat != lastBeat){ changeCounter++; }
-      totalCounter++;
+      
       lastBeat = heartBeat;
     
-      //if we get 10 changes in a span of 15 sec continue to healthy state
+      //if we get 5 changes in a span of 260ms continue to healthy state
       if (changeCounter > goal){
+        //sets everything up for the next state
+        changeCounter = 0;
+        waitPeriod = 52;
+        startTime = millis();
+        goal = 1; 
         state = 1;
       }
       
       //reset counters to continue waiting
-      if (totalCounter > waitPeriod){
-        totalCounter = 0;
+      if (currTime - startTime > waitPeriod){
+        startTime = millis();
         changeCounter = 0;
       }
       break;
       
     //HEALTHY STATE
     case 1:
-      //might want to add some tolerance here
       digitalWrite(outPin, 1);
       heartBeat = digitalRead(inPin);
-      if (heartBeat == lastBeat){
-        state = 2;
-      }
-    
+      currTime = millis();
+      //check for change in hearbeat
+      
+      if (heartBeat != lastBeat){ changeCounter++; }
+      
       lastBeat = heartBeat;
+      
+      if (currTime - startTime >= waitPeriod){
+        if (changeCounter < goal){
+          state = 2;
+        }
+        startTime = millis();
+        changeCounter = 0;
+      }
       break;
       
     //DEAD STATE
