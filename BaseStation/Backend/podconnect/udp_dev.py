cd091f89c17cb5e0ef699bbf18b6e191daa9ca39
Jack Harris <jhar23158@gmail.com>
2019-02-26 03:29:34 +0000

    Ping-ack: Created the basic ping-ack protocol

---
+++ BaseStation/Backend/podconnect/udp_dev.py
@@ -0,0 +1,20 @@
+import socket
+import time
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5005
+MESSAGE = "PING"
+
+print "UDP target IP:", UDP_IP
+print "UDP target port:", UDP_PORT
+
+sock2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+sock2.bind((UDP_IP, 5004))
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+
+while True:
+  sock.sendto(MESSAGE, (UDP_IP, 5005))
+
+  data, addr = sock2.recvfrom(1024) # buffer size is 1024 bytes
+  print "SENDERE: received message:", data
+  time.sleep(.9)
