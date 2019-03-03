0880748fed878104d35745a6bd7a91256ad666b7
Richard Wendel <rgw3d@aol.com>
2019-03-02 20:21:49 -0600

    Cleaned up the code a bit. Ready for application layer software

--- CentralComputing/CANManager.cpp
+++ CentralComputing/CANManager.cpp
@@ -16,17 +16,6 @@ bool CANManager::initialize_source(){
     return false;
   }
 
-  //int flags = fcntl(can_fd, F_GETFL, 0);
-  //if(flags == -1){
-  //  print(LogLevel::LOG_ERROR, "CAN F_GETFL failed. %s\n", strerror(errno));
-  //  return false;
-  //}
-  //flags |= O_NONBLOCK;
-  //if(fcntl(can_fd, F_SETFL, flags) == -1){
-  //  print(LogLevel::LOG_ERROR, "CAN F_SETFL failed. %s\n", strerror(errno));
-  //  return false;
-  //}
-
   // Convert interface string into interface index
   strcpy(ifr.ifr_name, "can0");
   if(ioctl(can_fd, SIOCGIFINDEX, &ifr) == -1){
@@ -44,6 +33,13 @@ bool CANManager::initialize_source(){
     return false;
   }
 
+  // Setup recv_frame() variables
+  iov.iov_base = &r_frame;
+  msg.msg_name = &addr;
+  msg.msg_iov = &iov;
+  msg.msg_iovlen = 1;
+  msg.msg_control = &ctrlmsg;
+
   print(LogLevel::LOG_DEBUG, "CAN Manger setup successful\n");
   return true;
 }
@@ -53,15 +49,13 @@ void CANManager::stop_source(){
   print(LogLevel::LOG_DEBUG, "CAN Manger stopped\n");
 }
 
-bool CANManager::send_frame(uint32_t can_id, const uint8_t * buf, int len){
+bool CANManager::send_frame(uint32_t can_id, const char * buf, int len){
   // Populate frame
-  struct can_frame frame;
-  memset(&frame, 0, sizeof(frame));
-  frame.can_id = can_id;
-  strncpy((char *)frame.data, (const char *)buf, (size_t)len);
-  frame.can_dlc = len;
-  // Write frame
-  int ret = write(can_fd, &frame, sizeof(frame));
+  s_frame.can_id = can_id;
+  strncpy((char *)s_frame.data, buf, (size_t)len);
+  s_frame.can_dlc = len;
+  // Write s_frame
+  int ret = write(can_fd, &s_frame, sizeof(s_frame));
   // Check for errors
   if(ret == -1){
     print(LogLevel::LOG_ERROR, "CAN send_frame failed. %s\n", strerror(errno));
@@ -75,18 +69,9 @@ bool CANManager::send_frame(uint32_t can_id, const uint8_t * buf, int len){
 }
 
 bool CANManager::recv_frame(){
-  struct iovec iov;
-  struct msghdr msg;
-  struct canfd_frame frame;
-  iov.iov_base = &frame;
-  msg.msg_name = &addr;
-  msg.msg_iov = &iov;
-  msg.msg_iovlen = 1;
-  msg.msg_control = &ctrlmsg;
   //https://stackoverflow.com/questions/32593697/understanding-the-msghdr-structure-from-sys-socket-h#32594071
 
-  //hotpath
-  iov.iov_len = sizeof(frame);
+  iov.iov_len = sizeof(r_frame);
   msg.msg_namelen = sizeof(addr);
   msg.msg_controllen = sizeof(ctrlmsg);
   msg.msg_flags = 0;
@@ -96,7 +81,7 @@ bool CANManager::recv_frame(){
   if(byte_count == -1){
     if(errno == EAGAIN || errno == EWOULDBLOCK){
       print(LogLevel::LOG_DEBUG, "CAN eagain ewouldblock. %s\n", strerror(errno));
-      return false;
+      return true;
     }
     else{
       print(LogLevel::LOG_ERROR, "CAN recvmsg failed. %s\n", strerror(errno));
@@ -104,38 +89,47 @@ bool CANManager::recv_frame(){
     }
   }
 
-  int maxdlen = 0;
-
-	if ((size_t)byte_count == CAN_MTU){
-    maxdlen = CAN_MAX_DLEN;
+  // Check to make sure we read a full message
+  // See linux/can.h for more details on these constants
+	if((size_t)byte_count == CAN_MTU){
+    print(LogLevel::LOG_DEBUG, "Can MTU\n");
   }
-  else if ((size_t)byte_count == CANFD_MTU){
-    maxdlen = CANFD_MAX_DLEN;
+  else if((size_t)byte_count == CANFD_MTU){
+    print(LogLevel::LOG_DEBUG, "CanFD MTU\n");
   }
   else {
     print(LogLevel::LOG_ERROR, "CAN recvmsg failed, incomplete CAN frame. \n");
     return false;
   }
 
-  char buff[64];
-  for(int j = 0; j < frame.len*2; j+=2){
-    put_hex_byte(buff+j, frame.data[j/2]);
-  }
-  buff[frame.len*2] = '\0';
-
-  print(LogLevel::LOG_INFO, "CAN msg: id: %d, len: %d, data: %s\n", frame.can_id, frame.len, buff);
 
   return true;
-
 }
 
 std::shared_ptr<CANData> CANManager::refresh() {
   std::shared_ptr<CANData> new_data = std::make_shared<CANData>();
-  new_data->dummy_data = i;
-  i++;
+  new_data->dummy_data = 2;
 
-  recv_frame();
-  send_frame(123, (const uint8_t *)"wow", 3);
+  //Send test frame 
+  if(!send_frame(100, "wow", 3)){
+    print(LogLevel::LOG_ERROR, "CAN send_frame failed. \n");
+    //TODO: put error on unified command queue
+  }
+
+  //Recieve frame. Populates variable r_frame
+  if(!recv_frame()){
+    print(LogLevel::LOG_ERROR, "CAN recv_frame failed. \n");
+    //TODO: put error on unified command queu
+  }
+
+  //Print the contents of r_frame (assumes len <= 8)
+  char buff[16];
+  for(int j = 0; j < r_frame.len*2; j+=2){
+    put_hex_byte(buff+j, r_frame.data[j/2]);
+  }
+  buff[r_frame.len*2] = '\0';//include null terminator
+
+  print(LogLevel::LOG_INFO, "CAN msg: id: %d, len: %d, data: %s\n", r_frame.can_id, r_frame.len, buff);
 
   return new_data;
 }
--- CentralComputing/CANManager.hpp
+++ CentralComputing/CANManager.hpp
@@ -30,22 +30,34 @@ class CANManager : public SourceManagerBase<(long long) (1.0 * 1E6), CANData, fa
     void stop_source();
     std::shared_ptr<CANData> refresh();
     std::shared_ptr<CANData> refresh_sim();
-    bool send_frame(uint32_t can_id, const uint8_t * buf, int len);
+    bool send_frame(uint32_t can_id, const char* buf, int len);
     bool recv_frame();
 
     // Heavily inspired by: https://github.com/linux-can/can-utils/blob/master/candump.c
     // https://www.can-cia.org/fileadmin/resources/documents/proceedings/2012_kleine-budde.pdf
+
+    // CAN socket
+    int can_fd;
+
+    // Used for CAN socket setup
     struct ifreq ifr;
     struct sockaddr_can addr;
-    char ctrlmsg[CMSG_SPACE(sizeof(struct timeval) + 3*sizeof(struct timespec) + sizeof(__u32))];
-    int can_fd;
+
+    //can_frame vs canfd_frame: https://computer-solutions.co.uk/info/Embedded_tutorials/can_tutorial.htm
+    //Note: canfd_frame is newer, but backwards compatibble with can_frame
+
+    // Send Frame. Used when calling write(). Note: type can_frame, not canfd_frame
+    struct can_frame s_frame;
+
+    // Recv Frame. Used when calling recvmsg(). Note: type canfd_frame, not can_frame
+    struct canfd_frame r_frame;
+    struct msghdr msg; // Used with recvmsg()
+    struct iovec iov;  // Necessary part of msg, although we don't use it
+    char ctrlmsg[CMSG_SPACE(1)]; // Necessary, although we dont use it. See: candump.c
 
     std::string name(){
       return "can";
     }
-
-    //iterator for testing purposes, remove
-    int i = 5;
 };
 
 #endif
