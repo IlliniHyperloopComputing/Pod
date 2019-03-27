5d9079c77e8f5b22d67e0091789075d2e3117aaa
Evan Paglieri <epaglier@purdue.edu>
2019-03-27 16:53:34 -0400

    Progress Bars added

--- BaseStation/frontend/src/App.js
+++ BaseStation/frontend/src/App.js
@@ -1,24 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import midwest from './midwest.png';
+import { Line, Circle } from 'rc-progress';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      speedPercent: 30,
+      speedColor: '#3FC7FA',
+      posPercent: 30,
+      posColor: '#3FC7FA',
+    };
+    this.changeState = this.changeState.bind(this);
+  }
+
+  changeState() {
+    const colorMap = ['#3FC7FA', '#85D262', '#FE8C6A'];
+    const speedValue = parseInt(Math.random() * 100, 10);
+    const posValue = parseInt(Math.random() * 100, 10);
+    this.setState({
+      speedPercent: speedValue,
+      speedColor: colorMap[parseInt(Math.random() * 3, 10)],
+      posPercent: posValue,
+    });
+  }
+
   render() {
+    const { speedPercent, speedColor, posPercent, posColor } = this.state;
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src={midwest} className="App-logo" alt="logo" />
+          <Circle percent={speedPercent} className="Speed-Circle" strokeWidth="4" strokeColor={speedColor}></Circle>
+          <Line percent={posPercent} strokeColor={posColor} className="Pos-Line"></Line>
+          <button onClick={this.changeState}></button>
         </header>
       </div>
     );
