a8d762b9098c7041c71e3e6235954491cf579671
Jack Harris <jhar23158@gmail.com>
2019-03-28 01:04:25 +0000

    Changed udpserver.py to be the code from udp_dev.py

--- BaseStation/Backend/podconnect/udpserver.py
+++ BaseStation/Backend/podconnect/udpserver.py
@@ -5,37 +5,27 @@ import socket
 from podconnect.models import DataPacket
 
 def serve():
-    UDP_IP = ''
-    UDP_PORT = 8002
-    BUFFER_SIZE = 20
+    UDP_IP = "127.0.0.1"
+    UDP_PORT = 5005
+    MESSAGE = "PING"
 
-    mutex = Lock()
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    bound = False
-    while (not bound):
-        try:
-            s.bind((UDP_IP, UDP_PORT))
-            bound = True
-        except:
-            UDP_PORT = UDP_PORT + 1
-    print("UDP Port = {port}".format(port=UDP_PORT))
+    sock2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+    sock2.bind(UDP_IP, 5004)
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+
+    sock2.settimeout(0.2) # Set timeout of recvfrom
+
     while (True):
-        while (True):
-            try:
-                data, addr = s.recvfrom(BUFFER_SIZE)
-                print(data)
-                if not data or data == None:
-                    break
-                data = data.decode()
-                dataSplit = data.split(',')
-                d = DataPacket(velocity=dataSplit[1],acceleration=dataSplit[2],position=dataSplit[3])
-                mutex.acquire(1)
-                d.save()
-                mutex.release()
-                print("UDP received data:", data, "From:", addr)
-            except:
-                print("Error in UDP received message")
+        sock.sendto(MESSAGE, (UDP_IP, 5005))
+        try:
+	    data, addr = sock2.recvfrom(1024) # buffer size is 1024 bytes
+	    print ("SENDERE: received message: ", data)
+	    time.sleep(.9)
+	except:
+	    print ("Never received message")
 
+                
 def start():
     t1 = Thread(target=serve)
     t1.start()
