01bb750f56c6377bf838898643232f4267bb1b87
Richard Wendel <rgw3d@aol.com>
2019-04-03 18:20:56 -0500

    Renaming refresh() to calculate, and having the state be passed in as a shared pointer. Removed uncessary filter functions :( , might use them again later but for now simple is better

--- CentralComputing/MotionModel.cpp
+++ CentralComputing/MotionModel.cpp
@@ -4,53 +4,21 @@ using Utils::print;
 using Utils::LogLevel;
 using Utils::microseconds;
 
-std::shared_ptr<MotionData> MotionModel::refresh() {
-  // Grab current time
-  int64_t cur_time = microseconds();
-  // Calculate time delta
-  //int64_t dt = cur_time - last_time;
-  // Set last time
-  //last_time = cur_time;
-
-  // Grab data
-  //std::shared_ptr<PRUData> pru = SourceManager::PRU.Get();
-  //std::shared_ptr<ADCData> adc = SourceManager::ADC.Get();
-
+void MotionModel::calculate(std::shared_ptr<UnifiedState> state){
   // Measured state variable
   MotionData meas;
-  //meas.x[0] = MotionModel::Median(pru.get()->encoder_distance, NUM_ENC_INPUTS);
-  //meas.x[1] = MotionModel::Median(pru.get()->encoder_velocity, NUM_ENC_INPUTS);
-  //meas.x[2] = MotionModel::Median(adc.get()->accel, NUM_ACCEL); 
-  //meas.rpm  = MotionModel::Median(pru.get()->disk_RPM, NUM_MOTOR_INPUTS);
-  //meas.fM = MotionModel::motor_profile(meas.x[1], meas.rpm);
-  //meas.fD = MotionModel::drag_profile(meas.x[1]);
+  meas.x[0] = Median(state->pru_data.get()->encoder_distance, NUM_ENC_INPUTS);
+  meas.x[1] = Median(state->pru_data.get()->encoder_velocity, NUM_ENC_INPUTS);
+  meas.x[2] = Median(state->adc_data.get()->accel, NUM_ACCEL); 
   
-
-
-  MotionData gain;
-  gain.x[0] = 1;
-  gain.x[1] = 1;
-  gain.x[2] = 1;
-  gain.rpm = 1;
-  gain.fM = 1;
-  gain.fD = 1;
-
-  // Apply Constant Gain Filter
-  // MotionModel::ConstantGainFilter(&state, meas, gain, dt);
-
   // Return state
   std::shared_ptr<MotionData> new_data = std::make_shared<MotionData>();
-  //*new_data = state;
-
-  return new_data;
 }
 
 // get MotionData object
-std::shared_ptr<MotionData> MotionModel::refresh_sim() {
+void MotionModel::calculate_sim(std::shared_ptr<UnifiedState> state){
   #ifdef SIM
-  return SimulatorManager::sim.sim_get_motion();
-  #else
-  return std::shared_ptr<MotionData>();
+  state->motion_data = SimulatorManager::sim.sim_get_motion();
   #endif
 }
 
@@ -58,54 +26,17 @@ double MotionModel::LowPass(double t_old, double t_new) {
   return t_old * LOWPASS_PERCENT + t_new * (1.0 - LOWPASS_PERCENT);
 }
 
-void MotionModel::ConstantGainFilter(MotionData * state, 
-                                const MotionData & meas, 
-                                const MotionData & gain,
-                                double delta) {
-  // Propagate state forward
-  MotionData estimate;
-  estimate.x[0] = state->x[0] + state->x[1] * delta;  // propagate distance 
-  estimate.x[1] = state->x[1] + state->x[2] * delta;  // propagate velocity
-  
-  // RPM should track velocity
-  // Take velocity slope (acceleration) and propagate forward
-  estimate.rpm = state->rpm + (state->x[2] * delta) * MS_TO_RPM;
-
-  // Calculate estimated Acceleration
-  estimate.fM = motor_profile(estimate.x[1], estimate.rpm);  // Motor force
-  estimate.fD = drag_profile(estimate.x[1]);  // Drag force
-
-  // Estimate acceleration based on input forces
-  estimate.x[2] = (state->fM + state->fD) / MASS;
-
-
-  // Calculate residual between measurment and estimate
-  MotionData residual;
-  residual.x[0] = meas.x[0] - estimate.x[0];
-  residual.x[1] = meas.x[1] - estimate.x[1];
-  residual.x[2] = meas.x[2] - estimate.x[2];
-  residual.fM   = meas.fM   - estimate.fM;
-  residual.fD   = meas.fD   - estimate.fD;
-  residual.rpm  = meas.rpm  - estimate.rpm;
-
-
-  // Calculate new state
-  state->x[0] = estimate.x[0] + gain.x[0] * residual.x[0];
-  state->x[1] = estimate.x[1] + gain.x[1] * residual.x[1];
-  state->x[2] = estimate.x[2] + gain.x[2] * residual.x[2];
-  state->fM   = estimate.fM   + gain.fM   * residual.fM;
-  state->fD   = estimate.fD   + gain.fD   * residual.fD;
-  state->rpm  = estimate.rpm  + gain.rpm  * residual.rpm;
-}
-
-double MotionModel::drag_profile(double velocity) {
-  // Apply estimated drag from aero/ friction/ wheel sources
-  // Should be based on velocity
-  return 0;
-}
-
-double MotionModel::motor_profile(double velocity, double RPM) {
-  // Based on profiling curve, calculate velocity.
-  // Maybe we just have a lookup table?
-  return 0;
+template<class T>
+T MotionModel::Median(T * input, unsigned size) {
+  // Is even. Need to find two indicies, 
+  // and then take the mean
+  if (size % 2 == 0) {
+    std::nth_element(input, input+size/2, input + size);
+    std::nth_element(input + size/2, input+size/2, input+size);
+    T sum = input[size/2] + input[size/2+1];
+    return sum/2;
+  } else {
+    std::nth_element(input, input+size/2+1, input + size);
+    return input[size/2 + 1];
+  }
 }
