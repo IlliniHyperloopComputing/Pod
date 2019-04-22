5a7e0c706640e9cf088fba9da32b6e2266a0b8ca
Richard Wendel <rgw3d@aol.com>
2019-04-22 15:01:58 -0500

    Formatting, and adding set_error_flag. It feels more natural here than in Utils

--- CentralComputing/Command.cpp
+++ CentralComputing/Command.cpp
@@ -1,6 +1,7 @@
 #include "Command.h"
 
 SafeQueue<uint16_t> Command::command_queue;
+int64_t Command::error_flag_timers[8*6]; 
 
 void Command::put(uint8_t id, uint8_t value) {
   uint16_t toQueue = (id << 8) | value;
@@ -16,3 +17,19 @@ bool Command::get(Network_Command * com) {
   com->id = static_cast<uint8_t>(preShift >> 8);
   return true;
 }
+
+// Used in set_error_flag to not flood the command queue
+// See the .h for more explanation
+void Command::set_error_flag(uint8_t id, uint8_t value){
+  for (int i = 0, j = 1; i < 8; i++, j*=2) {  // Go through each bit of the flag  
+    if (value & j) {  // if the specific bit is on
+      // Determine the time delta, use the error_flag_timers
+      int64_t delta = Utils::microseconds() - error_flag_timers[(id-Command::SET_ADC_ERROR) * 8 + i]; 
+      if (delta > 1000000) {  // Delta is greater than 1 second. This means we only send once per second!
+        error_flag_timers[(id-Command::SET_ADC_ERROR) * 8 + i] = Utils::microseconds();
+        Command::put(id, value & j);  // put command on queue
+      }
+    }
+  }
+}
+
