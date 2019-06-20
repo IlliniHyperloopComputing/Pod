2c5ddba3419443aa002ea20b958bbf53f933bc72
Anshul <anshuls3@illinois.edu>
2019-06-20 02:35:35 +0000

    changed a struct

--- CentralComputing/ADCManager.cpp
+++ CentralComputing/ADCManager.cpp
@@ -13,7 +13,6 @@ std::shared_ptr<ADCData> ADCManager::refresh() {
   // this is where you would query the ADC and get new data
   
   std::shared_ptr<ADCData> new_data = std::make_shared<ADCData>();
-  new_data->dummy_data = i;
   i++;
 
   return new_data;
