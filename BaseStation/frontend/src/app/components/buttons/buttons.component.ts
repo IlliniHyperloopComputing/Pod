afe66f39b17ae378491a7d9a98b6a293f673f0cb
Evan <epaglier@purdue.edu>
2019-06-21 00:15:16 -0400

    Barebones asthetics

--- BaseStation/frontend/src/app/components/buttons/buttons.component.ts
+++ BaseStation/frontend/src/app/components/buttons/buttons.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./buttons.component.css']
 })
 export class ButtonsComponent implements OnInit {
-
+  leftButtonName:string = "Ready";
+  rightButtonName:string = "E-Stop";
   constructor() { }
 
   ngOnInit() {
