25f244deceff69ab16f7966f15c32228ef8323b4
Richard Wendel <rgw3d@aol.com>
2019-06-24 10:13:07 -0400

    Adding the 'No Action' define

--- CentralComputing/Motor.cpp
+++ CentralComputing/Motor.cpp
@@ -10,21 +10,24 @@ Motor::Motor() {
 void Motor::enable_motors() {
   set_throttle(MOTOR_OFF);
   set_motor_state(true);
-  print(LogLevel::LOG_DEBUG, "Motors Enabled\n");
 }
 
 void Motor::disable_motors() {
   set_throttle(MOTOR_OFF);
   set_motor_state(false);
-  print(LogLevel::LOG_DEBUG, "Motors Disabled\n");
 }
 
 void Motor::set_motor_state(bool enable) {
   enabled = enable;
-  #ifdef SIM
-  SimulatorManager::sim.sim_motor_state(enable);
+  #ifdef NO_ACTION
+    #ifdef SIM
+    SimulatorManager::sim.sim_motor_state(enable);
+    #else
+    print(LogLevel::LOG_DEBUG, "NO_ACTION: Motors: %s\n", enable?"Enabled":"Disabled");
+    #endif
   #else
   SourceManager::CAN.set_motor_state(enable);
+  print(LogLevel::LOG_DEBUG, "Motors: %s\n", enable?"Enabled":"Disabled");
   #endif
 }
 
@@ -39,20 +42,28 @@ int16_t Motor::get_throttle() {
 void Motor::set_throttle(int16_t value) {
   if (enabled) {
     throttle = value;
-    #ifdef SIM
-    SimulatorManager::sim.sim_motor_set_throttle(value);
+    #ifdef NO_ACTION
+      #ifdef SIM
+      SimulatorManager::sim.sim_motor_set_throttle(value);
+      #else
+      print(LogLevel::LOG_DEBUG, "NO_ACTION: Setting motor throttle: %d\n", value);
+      #endif
     #else
     SourceManager::CAN.set_motor_throttle(value);
-    #endif
-    
     print(LogLevel::LOG_DEBUG, "Setting motor throttle: %d\n", value);
+    #endif
   }
 }
 
 void Motor::set_relay_state(HV_Relay_Select relay, HV_Relay_State state) {
-  #ifdef SIM
-  SimulatorManager::sim.sim_relay_state(relay, state);
+  #ifdef NO_ACTION
+    #ifdef SIM
+    SimulatorManager::sim.sim_relay_state(relay, state);
+    #else
+    print(LogLevel::LOG_DEBUG, "NO_ACTION: Relay %d %s\n", relay, state?"Enabled":"Disabled");
+    #endif
   #else
   SourceManager::CAN.set_relay_state(relay, state);
+  print(LogLevel::LOG_DEBUG, "Relay %d %s\n", relay, state?"Enabled":"Disabled");
   #endif
 }
