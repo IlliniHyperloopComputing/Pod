33ede2accdc2eefd659bf85538e486ced3b128b0
Evan <epaglier@purdue.edu>
2019-07-02 19:12:00 -0400

    Added dev stuff

--- BaseStation/frontend/src/app/components/diagnostics/diagnostics.component.ts
+++ BaseStation/frontend/src/app/components/diagnostics/diagnostics.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./diagnostics.component.css']
 })
 export class DiagnosticsComponent implements OnInit {
+  commands:string[] = ["test", "test2"];
+  currValue:number;
+  currCommand:string = this.commands[0];
 
   constructor() { }
 
   ngOnInit() {
+    console.log(this.commands[0]);
   }
 
+  onSelect(currCommand:string) {
+    this.currCommand = currCommand;
+  }
+
+  onEvent(currValue:number) {
+    this.currValue = currValue;
+  }
+
+  onSubmit() {
+    console.log(this.currCommand + " " + this.currValue)
+  }
 }
