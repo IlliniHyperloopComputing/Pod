f9b12cb872dfe294592069a19d75a0a416b53c3a
Richard Wendel <rgw3d@aol.com>
2019-07-13 18:17:26 -0400

    Fixed(??) the udp server. Didn't really do much, though

--- BaseStation/Backend/podconnect/udpserver.py
+++ BaseStation/Backend/podconnect/udpserver.py
@@ -7,10 +7,10 @@ import time, socket, queue
 COMMAND_QUEUE = queue.Queue()
 
 send_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
-UDP_RECV_IP = ''
-UDP_SEND_IP = "192.168.6.2"
-UDP_SEND_PORT = 5004
-UDP_RECV_PORT = 5005
+UDP_RECV_IP = '127.0.0.1'
+UDP_SEND_IP = '127.0.0.1'
+UDP_SEND_PORT = 5005
+UDP_RECV_PORT = 5004
 MSG_TO_SEND = "PING"
 MSG_TO_RECV = "ACK"
 
@@ -32,7 +32,9 @@ def serve():
         try:
             data, addr = recv_sock.recvfrom(1024) # buffer size is 1024 bytes
             if data.decode() == MSG_TO_RECV:
-                print ("UDP: received correct message: ", data.decode())
+                # wait a small ammount of time, as to not cause a flood of messages back and forth
+                e.wait( timeout=0.020); 
+                # print ("UDP: received correct message: ", data.decode())
             else:
                 print ("UDP: received incorrect message: ", data.decode())
             #    # TODO: Determine what to do in this case! 
