2dcd0c5fc0a6950a24469e56af2fa55ebf810da8
Evan <epaglier@purdue.edu>
2019-07-15 15:07:12 -0700

    Bug fix

--- BaseStation/Backend/podconnect/tcphelper.py
+++ BaseStation/Backend/podconnect/tcphelper.py
@@ -11,7 +11,7 @@ def bytes_to_int16(bytes, length):
         new_bytes = []
         for x in range(0, length):
                 new_bytes.append(int.from_bytes(
-                bytes[(x*2):(x*2+2)], byteorder='big', signed=True))
+                bytes[(x*2):(x*2+2)], byteorder='little', signed=True))
         return new_bytes
 
 def bytes_to_signed_int32(bytes, length):
