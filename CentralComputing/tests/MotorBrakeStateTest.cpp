c9cb4f342967a11d7291d27a191ca17f462eb0c9
Leo <leoyang429@gmail.com>
2019-09-19 19:41:03 +0000

    Auto Tests for motor and brake written

--- CentralComputing/tests/MotorBrakeStateTest.cpp
+++ CentralComputing/tests/MotorBrakeStateTest.cpp
@@ -1,59 +1,45 @@
 #ifdef SIM
 #include "PodTest.cpp"
+using std::make_shared;
 
-TEST_F(PodTest, SafeMode) {
+TEST_F(PodTest, CheckMotorBrakeStates) {
+
+  MoveState(Command::Network_Command_ID::TRANS_SAFE_MODE, E_States::ST_SAFE_MODE, true);
   EXPECT_FALSE(pod->state_machine->motor.is_enabled());
-  EXPECT_FALSE(pod->state_machine->brakes.is_enabled());
-}
+  EXPECT_FALSE(pod->state_machine->brake.is_enabled());
 
-TEST_F(PodTest, FunctionalTestOutside) {
+  MoveState(Command::Network_Command_ID::TRANS_FUNCTIONAL_TEST_OUTSIDE, E_States::ST_FUNCTIONAL_TEST_OUTSIDE, true);
   EXPECT_FALSE(pod->state_machine->motor.is_enabled());
-  EXPECT_FALSE(pod->state_machine->brakes.is_enabled());
-}
-
-TEST_F(PodTest, FunctionalTestOutside) {
+  EXPECT_FALSE(pod->state_machine->brake.is_enabled());
+  
+  MoveState(Command::Network_Command_ID::TRANS_LOADING, E_States::ST_LOADING, true);
   EXPECT_FALSE(pod->state_machine->motor.is_enabled());
-  EXPECT_FALSE(pod->state_machine->brakes.is_enabled());
-}
+  EXPECT_FALSE(pod->state_machine->brake.is_enabled());
 
-TEST_F(PodTest, Loading) {
+  MoveState(Command::Network_Command_ID::TRANS_Functional_TEST_INSIDE, E_States::ST_FUNCTIONAL_TEST_INSIDE, true)
   EXPECT_FALSE(pod->state_machine->motor.is_enabled());
-  EXPECT_FALSE(pod->state_machine->brakes.is_enabled());
-}
+  EXPECT_FALSE(pod->state_machine->brake.is_enabled());
 
-TEST_F(PodTest, FunctionalTestInside) {
-  EXPECT_FALSE(pod->state_machine->motor.is_enabled());
-  EXPECT_FALSE(pod->state_machine->brakes.is_enabled());
-}
-
-TEST_F(PodTest, LaunchReady) {
+  MoveState(Command::Network_Command_ID::TRANS_LAUNCH_READY, E_States::LAUNCH_READY, true);
   EXPECT_TRUE(pod->state_machine->motor.is_enabled());
-  EXPECT_FALSE(pod->state_machine->brakes.is_enabled());
-}
+  EXPECT_FALSE(pod->state_machine->brake.is_enabled());
 
-TEST_F(PodTest, FlightAccel) {
+  MoveState(Command::Network_Command_ID::TRANS_FLIGHT_ACCEL, E_States::ST_FLIGHT_ACCEL, true);
   EXPECT_TRUE(pod->state_machine->motor.is_enabled());
-  EXPECT_FALSE(pod->state_machine->brakes.is_enabled());
-}
+  EXPECT_FALSE(pod->state_machine->brake.is_enabled());
 
-TEST_F(PodTest, FlightCoast) {
+  MoveState(Command::Network_Command_ID::TRANS_FLIGHT_COAST, E_States::ST_FLIGHT_COAST, true);
   EXPECT_FALSE(pod->state_machine->motor.is_enabled());
-  EXPECT_FALSE(pod->state_machine->brakes.is_enabled());
-}
+  EXPECT_FALSE(pod->state_machine->brake.is_enabled());
 
-TEST_F(PodTest, FlightBrake) {
+  MoveState(Command::Network_Command_ID::TRANS_FLIGHT_BRAKE, E_States::ST_FLIGHT_BRAKE, true);
   EXPECT_FALSE(pod->state_machine->motor.is_enabled());
-  EXPECT_TRUE(pod->state_machine->brakes.is_enabled());
-}
+  EXPECT_TRUE(pod->state_machine->brake.is_enabled());
 
-TEST_F(PodTest, FlightAbort) {
+  MoveState(Command::Network_Command_ID::TRANS_ABORT, E_States::ST_FLIGHT_ABORT, true);
   EXPECT_FALSE(pod->state_machine->motor.is_enabled());
-  EXPECT_TRUE(pod->state_machine->brakes.is_enabled());
-}
+  EXPECT_TRUE(pod->state_machine->brake.is_enabled());
 
-TEST_F(PodTest, FlightBrake) {
-  EXPECT_FALSE(pod->state_machine->motor.is_enabled());
-  EXPECT_TRUE(pod->state_machine->brakes.is_enabled());
 }
 
 #endif
