1c9856144f5a6b644183d04197fae861b975d50e
leo <leoyang429@gmail.com>
2019-11-14 15:25:43 -0600

    Some Structs (till I2CData) of Defines.hpp exposed; arrays not exposed

--- CommandQueue/commandqueue/Command.cpp
+++ CommandQueue/commandqueue/Command.cpp
@@ -230,11 +230,92 @@ std::string Command::get_network_command_value_string(Network_Command * com) {
 
 #include <boost/python.hpp>
 BOOST_PYTHON_MODULE(command){
-  using namespace boost::python;
-  def("put", Command::put);
-  def("get", Command::get);
-  def("wait_for_empty", Command::wait_for_empty);
-  class_<Command::Network_Command>("Network_Command")
-          .def_readonly("id", &Command::Network_Command::id)
-          .def_readonly("value", &Command::Network_Command::value);
+        using namespace boost::python;
+
+        // basic Command_Queue functions
+        def("put", Command::put);
+        def("get", Command::get);
+        def("wait_for_empty", Command::wait_for_empty);
+
+        // exposes Command.h
+        class_<Command::Network_Command>("Network_Command")
+        .def_readwrite("id", &Command::Network_Command::id)
+        .def_readwrite("value", &Command::Network_Command::value);
+
+        // exposes Defines.hpp
+        class_<ADCData>("ADCData")
+        .def_readonly("data", &ADCData::data);
+        // array should be exposed in another way because python doesn't have array
+        // here temporarily use readonly
+
+        class_<CANData>("CANData")
+        .def_readwrite("status_word", &CANData::status_word)
+        .def_readwrite("position_val", &CANData::position_val)
+        .def_readwrite("torque_val", &CANData::torque_val)
+        .def_readwrite("controller_temp", &CANData::controller_temp)
+        .def_readwrite("motor_temp", &CANData::motor_temp)
+        .def_readwrite("dc_link_voltage", &CANData::dc_link_voltage)
+        .def_readwrite("logic_power_supply_voltage", &CANData::logic_power_supply_voltage)
+        .def_readwrite("current_demand", &CANData::current_demand)
+        .def_readwrite("motor_current_val", &CANData::motor_current_val)
+        .def_readwrite("electrical_angle", &CANData::electrical_angle)
+        .def_readwrite("phase_a_current", &CANData::phase_a_current)
+        .def_readwrite("phase_b_current", &CANData::phase_b_current)
+        .def_readwrite("internal_relay_state", &CANData::internal_relay_state)
+        .def_readwrite("relay_state", &CANData::relay_state)
+        .def_readwrite("rolling_counter", &CANData::rolling_counter)
+        .def_readwrite("fail_safe_state", &CANData::fail_safe_state)
+        .def_readwrite("pack_current", &CANData::pack_current)
+        .def_readwrite("pack_voltage_inst", &CANData::pack_voltage_inst)
+        .def_readwrite("pack_voltage_open", &CANData::pack_voltage_open)
+        .def_readwrite("pack_soc", &CANData::pack_soc)
+        .def_readwrite("pack_amphours", &CANData::pack_amphours)
+        .def_readwrite(" pack_resistance", &CANData:: pack_resistance)
+        .def_readwrite("pack_dod", &CANData::pack_dod)
+        .def_readwrite("pack_soh", &CANData::pack_soh)
+        .def_readwrite("current_limit_status", &CANData::current_limit_status)
+        .def_readwrite("max_pack_dcl", &CANData::max_pack_dcl)
+        .def_readwrite("avg_pack_current", &CANData::avg_pack_current)
+        .def_readwrite("highest_temp", &CANData::highest_temp)
+        .def_readwrite("highest_temp_id", &CANData::highest_temp_id)
+        .def_readwrite("avg_temp", &CANData::avg_temp)
+        .def_readwrite("internal_temp", &CANData::internal_temp)
+        .def_readwrite("low_cell_voltage", &CANData::low_cell_voltage)
+        .def_readwrite("low_cell_voltage_id", &CANData::low_cell_voltage_id)
+        .def_readwrite("high_cell_voltage", &CANData::high_cell_voltage)
+        .def_readwrite("high_cell_voltage_id", &CANData::high_cell_voltage_id)
+        .def_readwrite("low_cell_internalR", &CANData::low_cell_internalR)
+        .def_readwrite("low_cell_internalR_id", &CANData::low_cell_internalR_id)
+        .def_readwrite("high_cell_internalR", &CANData::high_cell_internalR)
+        .def_readwrite("high_cell_internalR_id", &CANData::high_cell_internalR_id)
+        .def_readwrite("power_voltage_input", &CANData::power_voltage_input)
+        .def_readwrite("dtc_status_one", &CANData::dtc_status_one)
+        .def_readwrite("dtc_status_two", &CANData::dtc_status_two)
+        .def_readwrite("adaptive_total_cap", &CANData::adaptive_total_cap)
+        .def_readwrite("adaptive_amphours", &CANData::adaptive_amphours)
+        .def_readwrite("adaptive_soc", &CANData::adaptive_soc);
+
+        class_<BMSCellBroadcastData>("BMSCellBroadcastData")
+        .def_readwrite("cell_id", &BMSCellBroadcastData::cell_id)
+        .def_readwrite("instant_voltage", &BMSCellBroadcastData::instant_voltage)
+        .def_readwrite("internal_resistance", &BMSCellBroadcastData::internal_resistance)
+        .def_readwrite("open_voltage", &BMSCellBroadcastData::open_voltage)
+        .def_readwrite("checksum", &BMSCellBroadcastData::checksum);
+
+        class_<BMSCells>("BMSCells")
+        .def_readonly("cell_data", &BMSCells::cell_data)
+        .def_readwrite("num_therms_enabled", &BMSCells::num_therms_enabled)
+        .def_readwrite("lowest_therm_value", &BMSCells::lowest_therm_value)
+        .def_readwrite("highest_therm_value", &BMSCells::highest_therm_value)
+        .def_readwrite("highest_therm_id", &BMSCells::highest_therm_id)
+        .def_readwrite("lowest_therm_id", &BMSCells::lowest_therm_id)
+        .def_readwrite("PADDING", &BMSCells::PADDING)
+        .def_readwrite("PADDING2", &BMSCells::PADDING2)
+        .def_readwrite("PADDING3", &BMSCells::PADDING3)
+        .def_readonly("therm_value", &BMSCells::therm_value);
+
+        class_<I2CData>("I2CData")
+        .def_readonly("temp", &I2CData::temp)
+        .def_readwrite("pressure_sensor", &I2CData::pressure_sensor)
+        .def_readwrite("temp_sensor", &I2CData::temp_sensor);
 }
--- CommandQueue/commandqueue/command.py
+++ CommandQueue/commandqueue/command.py
@@ -1,13 +1,51 @@
 #!/usr/bin/env python
 
 import command
+
 print("import success")
-command.put(100, 100)
-print("push success")
+
+# test command queue functions
 network_command = command.Network_Command()
-assert command.get(network_command)
-print("pop success")
-assert network_command.id == 100
-assert network_command.value == 100
-print(network_command.id)
-print(network_command.value)
+for x in range(100000):
+    command.put(x, x)
+
+for x in range(100000):
+    assert command.get(network_command)
+    assert network_command.id == x
+    assert network_command.value == x
+
+# test ADCData
+adc_data = command.ADCData()
+print("ADCData Class Available")
+
+# test CANData
+can_data = command.CANData()
+print("CANData Class Available")
+
+can_data.status_word = 37
+assert can_data.status_word == 37
+print("CANData Class Variables Accessible")
+
+# test BMSCellBroadcastData
+BMS_cell_broadcast_data = command.BMSCellBroadcastData()
+print("BMSCellBroadcastData Class Available")
+
+BMS_cell_broadcast_data.cell_id = 37
+assert BMS_cell_broadcast_data.cell_id == 37
+print("BMSCellBroadcastData Class Variables Accessible")
+
+# test BMSCells
+BMS_cells = command.BMSCells()
+print("BMSCells Class Available")
+
+BMS_cells.num_therms_enabled = 37
+assert BMS_cells.num_therms_enabled == 37
+print("BMSCells Class Variables Accessible")
+
+# test I2CData
+I2C_data = command.I2CData()
+print("I2CData Class Available")
+
+I2C_data.pressure_sensor = 37
+assert I2C_data.pressure_sensor == 37
+print("I2CData Class Variables Accessible")
